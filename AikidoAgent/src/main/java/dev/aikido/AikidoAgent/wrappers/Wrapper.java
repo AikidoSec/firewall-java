7fc2bef9e4eb79698569fd93c5eed901cce332ff
Wout Feys <wout@aikido.dev>
2024-10-11 16:48:25 +0200

    Create abstract wrapper class and cleanup other wrappers

---
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/wrappers/Wrapper.java
@@ -0,0 +1,9 @@
+package dev.aikido.AikidoAgent.wrappers;
+
+import net.bytebuddy.asm.AsmVisitorWrapper;
+
+public abstract class Wrapper {
+    public static AsmVisitorWrapper get() {
+        return null;
+    }
+}
