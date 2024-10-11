b8ee444e1e48ebe1c96af12f633d81398534d76d
Wout Feys <wout@aikido.dev>
2024-10-11 23:21:50 +0200

    Extract HTTP response status code

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/wrappers/SpringFrameworkWrapper.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/wrappers/SpringFrameworkWrapper.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.aikido.AikidoAgent.context.ContextObject;
 import dev.aikido.AikidoAgent.context.SpringContextObject;
+import jakarta.servlet.http.HttpServletResponse;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.AsmVisitorWrapper;
 import net.bytebuddy.matcher.ElementMatchers;
@@ -22,12 +23,20 @@ public class SpringFrameworkWrapper extends Wrapper {
 
     private static class SpringFrameworkAdvice {
         @Advice.OnMethodEnter
-        public static void intercept(@Advice.Argument(0) HttpServletRequest request) {
+        public static HttpServletResponse interceptOnEnter(
+                @Advice.Argument(0) HttpServletRequest request,
+                @Advice.Argument(1) HttpServletResponse response) {
             ContextObject contextObject = new SpringContextObject(request);
 
             System.out.printf("Url: %s with Method: %s \n", contextObject.getUrl(), contextObject.getMethod());
             System.out.println(contextObject.toJson());
+            return response;
+        }
 
+        @Advice.OnMethodExit
+        public static void interceptOnExit(@Advice.Enter HttpServletResponse response) {
+            int statusCode = response.getStatus();
+            System.out.println("HTTP Status Code: " + statusCode);
         }
     }
 }
