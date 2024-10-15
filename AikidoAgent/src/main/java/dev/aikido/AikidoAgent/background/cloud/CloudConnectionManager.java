9c83429d0006efd29bb0453579b0bd582d13b92a
Wout Feys <wout@aikido.dev>
2024-10-15 13:04:46 +0200

    Add a getManagerInfo and some props to cloudconnectionmanager

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/cloud/CloudConnectionManager.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/cloud/CloudConnectionManager.java
@@ -1,4 +1,23 @@
 package dev.aikido.AikidoAgent.background.cloud;
 
 public class CloudConnectionManager {
+    private boolean blockingEnabled;
+    private String serverless;
+    public CloudConnectionManager(boolean block, String serverless) {
+        if (serverless != null && serverless.isEmpty()) {
+            throw new IllegalArgumentException("Serverless cannot be an empty string");
+        }
+        this.blockingEnabled = block;
+        this.serverless = serverless;
+    }
+    public boolean shouldBlock() {
+        return this.blockingEnabled;
+    }
+
+    public String getServerless() {
+        return serverless;
+    }
+    public GetManagerInfo.ManagerInfo getManagerInfo() {
+        return GetManagerInfo.getManagerInfo(this);
+    }
 }
