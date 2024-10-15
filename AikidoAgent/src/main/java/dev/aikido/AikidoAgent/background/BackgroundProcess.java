22659bf40fa5ef86d101bc51b55ca16127d7850f
Wout Feys <wout@aikido.dev>
2024-10-15 09:51:44 +0200

    Background Thread starts an IPC Server

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/BackgroundProcess.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/BackgroundProcess.java
@@ -1,14 +1,28 @@
 package dev.aikido.AikidoAgent.background;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.lang.Thread.sleep;
+
 public class BackgroundProcess extends Thread {
     public BackgroundProcess(String name) {
         super(name);
     }
 
     public void run() {
-            if (!Thread.currentThread().isDaemon()) {
+        if (!Thread.currentThread().isDaemon()) {
             return; // Can only run if thread is daemon
         }
         System.out.println("Background thread here!");
+        Path socketPath = Path
+            .of(System.getProperty("user.home"))
+            .resolve("aikido2342.socket");
+        try {
+            IPCServer server = new IPCServer(socketPath);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            this.start(); // Restart thread
+        }
     }
 }
