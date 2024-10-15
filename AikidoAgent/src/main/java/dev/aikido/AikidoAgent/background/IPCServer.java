26f5dc795ed47f43d0b4bea1c757f3dd96b3cf50
Wout Feys <wout@aikido.dev>
2024-10-15 09:51:57 +0200

    Update IPCServer with all logic to receive input

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/IPCServer.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/IPCServer.java
@@ -1,15 +1,43 @@
 package dev.aikido.AikidoAgent.background;
 
 import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 public class IPCServer {
+    private final ServerSocketChannel serverChannel;
+    public IPCServer(Path socketPath) throws IOException, InterruptedException {
+        // Delete previous socket file :
+        Files.deleteIfExists(socketPath); // Make sure this is alright with multiple agents
+
+        // Create a new server socket channel :
+        UnixDomainSocketAddress socketAddress = UnixDomainSocketAddress.of(socketPath);
+        this.serverChannel = ServerSocketChannel.open(StandardProtocolFamily.UNIX);
+        serverChannel.bind(socketAddress);
+        this.listen();
+
+    }
+    private void listen() throws IOException, InterruptedException {
+        while (true) {
+            System.out.println("Waiting for connection..");
+            SocketChannel channel = serverChannel.accept();
+            while (true) {
+                readSocketMessage(channel)
+                        .ifPresent(message -> System.out.printf("[Client message] %s", message));
+                Thread.sleep(100);
+            }
+        }
+    }
     private Optional<String> readSocketMessage(SocketChannel channel) throws IOException {
         // Create 1024 bytes long buffer :
         ByteBuffer buffer = ByteBuffer.allocate(1024);
-        
+
         // Check if bytes are empty :
         int bytesRead = channel.read(buffer);
         if (bytesRead < 0)
