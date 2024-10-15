aa2375c2d02cf43b677ea62414a747bf75a95224
Wout Feys <wout@aikido.dev>
2024-10-15 14:25:31 +0200

    Add dialects for SQL

---
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/vulnerabilities/sql_injection/Dialect.java
@@ -0,0 +1,20 @@
+package dev.aikido.AikidoAgent.vulnerabilities.sql_injection;
+
+import java.util.Objects;
+
+public class Dialect {
+    private final int rustDialectInt;
+    public Dialect(String dialect) {
+        if (Objects.equals(dialect, "postgres")) {
+            rustDialectInt = 9;
+        } else if (Objects.equals(dialect, "mysql")) {
+            rustDialectInt = 8;
+        } else {
+            rustDialectInt = -1;
+        }
+    }
+
+    public int getDialectInteger() {
+        return rustDialectInt;
+    }
+}
