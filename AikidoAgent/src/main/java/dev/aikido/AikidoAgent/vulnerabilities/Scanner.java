8dee37509a1c84fdff55223e552447a173d1be7b
Wout Feys <wout@aikido.dev>
2024-10-16 10:30:55 +0200

    Update Scanner.java with template

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/vulnerabilities/Scanner.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/vulnerabilities/Scanner.java
@@ -1,13 +1,28 @@
 package dev.aikido.AikidoAgent.vulnerabilities;
 
+import dev.aikido.AikidoAgent.context.Context;
+import dev.aikido.AikidoAgent.context.ContextObject;
+
 public class Scanner {
     public static void run(Attacks.Attack attack, String operation, String[] arguments) {
-        String userInput = "banana";
-        boolean isInjection = attack.getDetector().run(userInput, arguments);
+        ContextObject ctx = Context.get();
+        String[] userInputs = {}; // Update in later PR
+        boolean isInjection = false;
+        try {
+            for (String userInput : userInputs) {
+                if (attack.getDetector().run(userInput, arguments)) {
+                    // Attack detected, set injection and break for loop.
+                    isInjection = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Temporary logging measure
+        }
         if (isInjection) {
             // Report to background :
-            
-            // Throw error
+
+            // Throw error :
             throw new RuntimeException("SQL Injection");
         }
     }
