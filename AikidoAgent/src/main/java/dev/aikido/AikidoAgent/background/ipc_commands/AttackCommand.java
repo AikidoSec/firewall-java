ee755b5cd5afe4f795cb8f1b411b2a159060b802
Wout Feys <wout@aikido.dev>
2024-10-16 13:57:16 +0200

    Create an API Event in ATTACK command

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/AttackCommand.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/AttackCommand.java
@@ -1,6 +1,15 @@
 package dev.aikido.AikidoAgent.background.ipc_commands;
 
+import com.google.gson.Gson;
+import dev.aikido.AikidoAgent.background.cloud.CloudConnectionManager;
+import dev.aikido.AikidoAgent.background.cloud.api.events.APIEvent;
+import dev.aikido.AikidoAgent.background.cloud.api.events.DetectedAttack;
+import dev.aikido.AikidoAgent.context.ContextObject;
+import dev.aikido.AikidoAgent.vulnerabilities.Attack;
+
 public class AttackCommand implements Command {
+    private record AttackCommandData(Attack attack, ContextObject context) {}
+
     @Override
     public boolean returnsData() {
         return false;
@@ -12,7 +21,16 @@ public class AttackCommand implements Command {
     }
 
     @Override
-    public void execute(String data) {
-
+    public void execute(String data, CloudConnectionManager connectionManager) {
+        Gson gson = new Gson();
+        AttackCommandData attackCommandData = gson.fromJson(data, AttackCommandData.class);
+        if (attackCommandData.attack == null || attackCommandData.context == null) {
+            System.out.println("Attack or context not defined correctly, returning.");
+            return;
+        }
+        // Generate an attack event :
+        APIEvent detectedAttack = DetectedAttack.createAPIEvent(
+                attackCommandData.attack, attackCommandData.context, connectionManager
+        );
     }
 }
