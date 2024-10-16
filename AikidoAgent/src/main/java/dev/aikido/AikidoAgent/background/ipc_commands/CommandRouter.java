6b7936f99c7920b473870a7a293de1e95492c810
Wout Feys <wout@aikido.dev>
2024-10-16 13:18:31 +0200

    CommandRouter, seperate command from JSON

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/CommandRouter.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/CommandRouter.java
@@ -7,8 +7,21 @@ public class CommandRouter {
     public CommandRouter() {
         // Do some funky business
     }
+
+    /**
+     * Parses input string as an IPC command.
+     * @param input raw IPC command.
+     * -> E.g. input = "ATTACK${'this': 'that'}"
+     */
     public void parseIPCInput(String input) {
-        // Parse the funky Ipc input
-        System.out.println(input);
+        // P
+        int indexOfCommandSeparator = input.indexOf('$');
+        if (indexOfCommandSeparator == -1) {
+            System.out.println("Error : Separator not found.");
+            return;
+        }
+        String command = input.substring(0, indexOfCommandSeparator);
+        String data = input.substring(indexOfCommandSeparator + 1);
+        System.out.println("Command: " + command + "; Data: " + data);
     }
 }
