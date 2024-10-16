c60afb741f96fa28bc995b5bf08efd8467508bf5
Wout Feys <wout@aikido.dev>
2024-10-16 14:35:13 +0200

    Add comment explaining command interface

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/Command.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ipc_commands/Command.java
@@ -2,6 +2,10 @@ package dev.aikido.AikidoAgent.background.ipc_commands;
 
 import dev.aikido.AikidoAgent.background.cloud.CloudConnectionManager;
 
+/**
+ * Command interface for IPC Commands
+ * E.g. AttackCommand : gives false in returnsData(), matches it's name for "ATTACK" and executes code to report attacks.
+ */
 public interface Command {
     public boolean returnsData();
     public boolean matchesName(String command);
