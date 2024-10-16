c91f728d57031777f090e9c5b32c359247d02b10
Wout Feys <wout@aikido.dev>
2024-10-16 11:06:32 +0200

    Run attack function in SQL Collector

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/collectors/SQLCollector.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/collectors/SQLCollector.java
@@ -1,7 +1,12 @@
 package dev.aikido.AikidoAgent.collectors;
 
+import dev.aikido.AikidoAgent.vulnerabilities.Attacks;
+import dev.aikido.AikidoAgent.vulnerabilities.Scanner;
+
 public class SQLCollector {
-    public static void report(String sql, String dialect) {
+    public static void report(String sql, String dialect, String operation) {
         System.out.println("SQL : " + sql + ", Dialect : " + dialect);
+        Attacks.Attack attack = new Attacks.SQLInjection();
+        Scanner.run(attack, operation, new String[]{sql, dialect});
     }
 }
