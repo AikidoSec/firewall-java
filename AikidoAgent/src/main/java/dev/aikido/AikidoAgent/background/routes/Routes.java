c223b14f0c6c8c960f316fc1c401c674a1b97cae
Wout Feys <wout@aikido.dev>
2024-10-18 10:27:21 +0200

    Create RouteEntry and Routes class

---
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/routes/Routes.java
@@ -0,0 +1,75 @@
+package dev.aikido.AikidoAgent.background.routes;
+import dev.aikido.AikidoAgent.context.RouteMetadata;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import static dev.aikido.AikidoAgent.background.routes.RouteToKeyHelper.routeToKey;
+
+public class Routes {
+    private final int maxSize;
+    private final Map<String, RouteEntry> routes;
+
+    public Routes(int maxSize) {
+        this.maxSize = maxSize;
+        this.routes = new HashMap<>();
+    }
+
+    public Routes() {
+        this(1000); // Default max size
+    }
+
+    public void initializeRoute(RouteMetadata routeMetadata) {
+        manageRoutesSize();
+        String key = routeToKey(routeMetadata);
+        if (routes.containsKey(key)) {
+            return;
+        }
+        routes.put(key, new RouteEntry(routeMetadata));
+    }
+
+    public void incrementRoute(RouteMetadata routeMetadata) {
+        String key = routeToKey(routeMetadata);
+        RouteEntry route = routes.get(key);
+        if (route == null) {
+            return;
+        }
+        route.incrementHits();
+    }
+
+    public RouteEntry get(RouteMetadata routeMetadata) {
+        String key = routeToKey(routeMetadata);
+        return routes.get(key);
+    }
+
+    public void clear() {
+        routes.clear();
+    }
+
+    private void manageRoutesSize() {
+        if (routes.size() >= maxSize) {
+            String leastUsedKey = null;
+            int leastHits = Integer.MAX_VALUE;
+
+            for (Map.Entry<String, RouteEntry> entry : routes.entrySet()) {
+                if (entry.getValue().getHits() < leastHits) {
+                    leastUsedKey = entry.getKey();
+                    leastHits = entry.getValue().getHits();
+                }
+            }
+
+            if (leastUsedKey != null) {
+                routes.remove(leastUsedKey);
+            }
+        }
+    }
+
+    public Iterator<RouteEntry> iterator() {
+        return routes.values().iterator();
+    }
+
+    public int size() {
+        return routes.size();
+    }
+}
