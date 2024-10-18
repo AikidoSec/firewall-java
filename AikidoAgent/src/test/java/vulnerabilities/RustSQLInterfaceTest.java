2594137c32cf0ec4dba06ed294402cf3252745a5
Wout Feys <wout@aikido.dev>
2024-10-18 15:00:57 +0200

    Add SQL/Rust tests

--- AikidoAgent/src/test/java/vulnerabilities/RustSQLInterfaceTest.java
+++ AikidoAgent/src/test/java/vulnerabilities/RustSQLInterfaceTest.java
@@ -10,11 +10,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class RustSQLInterfaceTest {
     @Test
     public void testItWorks() {
-        /*
         boolean injectionResult = RustSQLInterface.detectSqlInjection("SELECT * FROM table;", "table;", new Dialect("postgres"));
         assertTrue(injectionResult);
         injectionResult = RustSQLInterface.detectSqlInjection("SELECT * FROM table;", "table", new Dialect("postgres"));
         assertFalse(injectionResult);
-         */
     }
 }
