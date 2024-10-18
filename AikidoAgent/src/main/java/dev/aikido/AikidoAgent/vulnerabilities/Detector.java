a5797762d2625bc728965f3e90d2a1d43a39b71d
Wout Feys <wout@aikido.dev>
2024-10-18 16:03:43 +0200

    Make SqlDetector use DetectorResult

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/vulnerabilities/Detector.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/vulnerabilities/Detector.java
@@ -1,5 +1,27 @@
 package dev.aikido.AikidoAgent.vulnerabilities;
 
+import java.util.Map;
+
 public interface Detector {
-    boolean run(String userInput, String[] arguments);
+    DetectorResult run(String userInput, String[] arguments);
+    public static class DetectorResult {
+        private final boolean detectedAttack;
+        private final Map<String, String> metadata;
+        public DetectorResult(boolean detectedAttack, Map<String, String> metadata) {
+            this.detectedAttack = detectedAttack;
+            this.metadata = metadata;
+        }
+        public DetectorResult() {
+            this.detectedAttack = false;
+            this.metadata = null;
+        }
+
+        public boolean isDetectedAttack() {
+            return detectedAttack;
+        }
+
+        public Map<String, String> getMetadata() {
+            return metadata;
+        }
+    }
 }
