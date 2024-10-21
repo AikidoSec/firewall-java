a4ca2f384f9608d79c31ed732d71786cbf794a30
Wout Feys <wout@aikido.dev>
2024-10-21 15:03:32 +0200

    Create new getters, add new fields and fetch new fields from API

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ServiceConfiguration.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/ServiceConfiguration.java
@@ -2,6 +2,10 @@ package dev.aikido.AikidoAgent.background;
 
 import dev.aikido.AikidoAgent.background.cloud.api.APIResponse;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * This class holds all config objects from Aikido's servers, i.e. endpoints, blocked IPs, bypassed users, ...
  * It is essential for e.g. rate limiting
@@ -9,25 +13,40 @@ import dev.aikido.AikidoAgent.background.cloud.api.APIResponse;
 public class ServiceConfiguration {
     private final String serverless;
     private boolean blockingEnabled;
+    private HashSet<String> bypassedIPs =  new HashSet<>();
+    private HashSet<String> blockedUserIDs = new HashSet<>();
+    private List<Endpoint> endpoints = new ArrayList<>();
     public ServiceConfiguration(boolean blockingEnabled, String serverless) {
         if (serverless != null && serverless.isEmpty()) {
             throw new IllegalArgumentException("Serverless cannot be an empty string");
         }
         this.blockingEnabled = blockingEnabled;
         this.serverless = serverless;
+
     }
     public void updateConfig(APIResponse apiResponse) {
         if (apiResponse == null || !apiResponse.success()) {
             return;
         }
         this.blockingEnabled = apiResponse.block();
+        this.bypassedIPs = new HashSet<>(apiResponse.allowedIPAddresses());
+        this.blockedUserIDs = new HashSet<>(apiResponse.blockedUserIds());
+        this.endpoints = apiResponse.endpoints();
     }
-
+    // Getters :
     public String getServerless() {
         return serverless;
     }
-
     public boolean isBlockingEnabled() {
         return blockingEnabled;
+    }
+    public HashSet<String> getBypassedIPs() {
+        return bypassedIPs;
+    }
+    public List<Endpoint> getEndpoints() {
+        return endpoints;
+    }
+    public HashSet<String> getBlockedUserIDs() {
+        return blockedUserIDs;
     }
 }
