d833ca0e37a89fba58550c4c73721a2b5c03429b
Wout Feys <wout@aikido.dev>
2024-10-21 15:11:54 +0200

    Add allowed IPs to the Endpoint class

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/Endpoint.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/background/Endpoint.java
@@ -1,14 +1,18 @@
 package dev.aikido.AikidoAgent.background;
 
+import java.util.List;
+
 public class Endpoint {
     public record RateLimitingConfig(long maxRequests, long windowSizeInMS) {}
     private final String method;
     private final String route;
     private final RateLimitingConfig rateLimiting;
-    public Endpoint(String method, String route, long maxRequests, long windowSizeMS) {
+    private final List<String> allowedIPAddresses;
+    public Endpoint(String method, String route, long maxRequests, long windowSizeMS, List<String> allowedIPAddresses) {
         this.method = method;
         this.route = route;
         this.rateLimiting = new RateLimitingConfig(maxRequests, windowSizeMS);
+        this.allowedIPAddresses = allowedIPAddresses;
     }
 
     // Getters :
@@ -20,5 +24,8 @@ public class Endpoint {
     }
     public RateLimitingConfig getRateLimiting() {
         return rateLimiting;
+    }
+    public List<String> getAllowedIPAddresses() {
+        return allowedIPAddresses;
     }
 }
