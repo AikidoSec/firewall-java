207409ac95bf874e3465736bc891f7caab513cf3
Wout Feys <wout@aikido.dev>
2024-10-22 14:07:49 +0200

    Allow for updating code after import, Allow for safer wrapping

--- AikidoAgent/src/main/java/dev/aikido/AikidoAgent/wrappers/SpringFrameworkWrapper.java
+++ AikidoAgent/src/main/java/dev/aikido/AikidoAgent/wrappers/SpringFrameworkWrapper.java
@@ -7,39 +7,56 @@ import dev.aikido.AikidoAgent.context.ContextObject;
 import dev.aikido.AikidoAgent.context.SpringContextObject;
 import jakarta.servlet.http.HttpServletResponse;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.asm.AsmVisitorWrapper;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Executable;
+
 import static dev.aikido.AikidoAgent.helpers.url.IsUsefulRoute.isUsefulRoute;
 
-public class SpringFrameworkWrapper extends Wrapper {
+public class SpringFrameworkWrapper implements Wrapper {
     public static final Logger logger = LogManager.getLogger(SpringFrameworkWrapper.class);
-    public static AsmVisitorWrapper get() {
+
+    @Override
+    public String getName() {
         // We wrap the function processRequest which gets called with
         // HttpServletRequest request, HttpServletResponse response
         // And is part of org.springframework.web.servlet.FrameworkServlet
         // See : https://github.com/spring-projects/spring-framework/blob/eb4bf1c0a65db32a161abd6fc89c69623dd80418/spring-webmvc/src/main/java/org/springframework/web/servlet/FrameworkServlet.java#L996
-        return Advice.to(SpringFrameworkAdvice.class)
-                .on(ElementMatchers.named("processRequest"));
+        return SpringFrameworkAdvice.class.getName();
+    }
+    @Override
+    public ElementMatcher<? super MethodDescription> getMatcher() {
+        return ElementMatchers.nameContainsIgnoreCase("processRequest");
     }
 
     private static class SpringFrameworkAdvice {
         @Advice.OnMethodEnter
         public static HttpServletResponse interceptOnEnter(
-                @Advice.Argument(0) HttpServletRequest request,
-                @Advice.Argument(1) HttpServletResponse response) {
-            Context.reset();
-            ContextObject contextObject = new SpringContextObject(request);
-            Context.set(contextObject);
-            return response;
+                @Advice.Origin Executable method,
+                @Advice.Argument(0) Object request,
+                @Advice.Argument(1) Object response) {
+            try {
+                Context.reset();
+                ContextObject contextObject = new SpringContextObject((HttpServletRequest) request);
+                Context.set(contextObject);
+                return (HttpServletResponse) response;
+            } catch (Throwable e) {
+                logger.debug(e);
+            }
+            return null;
         }
 
         @Advice.OnMethodExit
         public static void interceptOnExit(@Advice.Enter HttpServletResponse response) {
+            if (response == null) {
+                return;
+            }
             int statusCode = response.getStatus();
             ContextObject context = Context.get();
             boolean currentRouteUseful = isUsefulRoute(statusCode, context.getRoute(), context.getMethod());
