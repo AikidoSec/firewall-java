a119287c1e8cbd58578282c7958e810a776e4b46
Wout Feys <wout@aikido.dev>
2024-10-23 18:25:42 +0200

    Upgrade UnixTimeMS to be more accurate

--- agent_api/src/main/java/dev/aikido/agent_api/helpers/UnixTimeMS.java
+++ agent_api/src/main/java/dev/aikido/agent_api/helpers/UnixTimeMS.java
@@ -4,6 +4,6 @@ import java.time.Instant;
 
 public class UnixTimeMS {
     public static long getUnixTimeMS() {
-        return Instant.now().getEpochSecond() * 1000;
+        return Instant.now().toEpochMilli();
     }
 }
