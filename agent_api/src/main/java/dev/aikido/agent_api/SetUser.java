902be0f45f03adf825ee4fc95a94a63bdd2e354a
Wout Feys <wout@aikido.dev>
2024-10-29 17:10:15 +0100

    Create a setUser function

---
+++ agent_api/src/main/java/dev/aikido/agent_api/SetUser.java
@@ -0,0 +1,30 @@
+package dev.aikido.agent_api;
+
+import dev.aikido.agent_api.context.Context;
+import dev.aikido.agent_api.context.ContextObject;
+import dev.aikido.agent_api.context.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SetUser {
+    private static final Logger logger = LogManager.getLogger(SetUser.class);
+
+    public record UserObject(String id, String name) {}
+    public static void setUser(UserObject user) {
+        if(user.id().isEmpty() || user.name().isEmpty()) {
+            logger.info("User ID or name cannot be empty.");
+            return;
+        }
+        ContextObject currentContext = Context.get();
+        if (currentContext == null) {
+            return;
+        }
+        if (currentContext.middlewareExecuted()) {
+            logger.info(
+                "setUser(...) must be called before the Zen middleware is executed."
+            );
+        }
+        User validatedUser = new User(user.id(), user.name(), currentContext.getRemoteAddress());
+        currentContext.setUser(validatedUser);
+    }
+}
---
+++ agent_api/src/main/java/dev/aikido/agent_api/context/User.java
@@ -0,0 +1,4 @@
+package dev.aikido.agent_api.context;
+
+public record User(String id, String name, String lastIpAddress) {
+}
