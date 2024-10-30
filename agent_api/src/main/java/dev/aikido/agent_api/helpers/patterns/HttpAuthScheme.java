081e114cc8c88ac4bf777dfddb8a4a55af59fa6b
Wout Feys <wout@aikido.dev>
2024-10-30 15:39:07 +0100

    Port over code for GetAuthTypes

---
+++ agent_api/src/main/java/dev/aikido/agent_api/helpers/patterns/HttpAuthScheme.java
@@ -0,0 +1,18 @@
+package dev.aikido.agent_api.helpers.patterns;
+
+public class HttpAuthScheme {
+    private static final String[] AUTH_SCHEMES = {
+        "basic",  "bearer",  "digest",
+        "dpop", "gnap", "hoba",
+        "mutual", "negotiate", "privatetoken",
+        "scram-sha-1", "scram-sha-256", "vapid"
+    };
+    public static boolean isHttpAuthScheme(String scheme) {
+        for (String authScheme: AUTH_SCHEMES) {
+            if(authScheme.equalsIgnoreCase(scheme)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
