18412140032af6ce8743633d2542f0cd5e437f6a
Wout Feys <wout@aikido.dev>
2024-10-31 16:51:49 +0100

    Add hostnames to thread cache object

--- agent_api/src/main/java/dev/aikido/agent_api/collectors/URLCollector.java
+++ agent_api/src/main/java/dev/aikido/agent_api/collectors/URLCollector.java
@@ -1,5 +1,7 @@
 package dev.aikido.agent_api.collectors;
 
+import dev.aikido.agent_api.thread_cache.ThreadCache;
+import dev.aikido.agent_api.thread_cache.ThreadCacheObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,6 +10,10 @@ import java.net.URL;
 public class URLCollector {
     private static final Logger logger = LogManager.getLogger(URLCollector.class);
     public static void report(URL url) {
-        logger.debug("URL Reported, with port: {}; Hostname: {}", url.getPort(), url.getHost());
+        ThreadCacheObject threadCache = ThreadCache.get();
+        if(threadCache != null) {
+            threadCache.getHostnames().add(url.getHost(), url.getPort());
+            ThreadCache.set(threadCache);
+        }
     }
 }
