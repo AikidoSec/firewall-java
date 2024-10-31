698382f2a72e889f570384f2041e8fc94630fcc2
Wout Feys <wout@aikido.dev>
2024-10-31 10:38:04 +0100

    Add unit tests to Resolver function

---
+++ agent_api/src/test/java/vulnerabilities/ssrf/ResolverTest.java
@@ -0,0 +1,64 @@
+package vulnerabilities.ssrf;
+
+import dev.aikido.agent_api.vulnerabilities.ssrf.imds.Resolver;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static dev.aikido.agent_api.vulnerabilities.ssrf.imds.Resolver.resolvesToImdsIp;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ResolverTest {
+    @Test
+    void testResolvesToImdsIp_WithTrustedHostname() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("192.168.1.1"); // Non-IMDS IP
+
+        assertFalse(Resolver.resolvesToImdsIp(resolvedIps, "metadata.google.internal"));
+    }
+
+    @Test
+    void testResolvesToImdsIp_WithImdsIp() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("169.254.169.254"); // IMDS IP
+
+        assertTrue(Resolver.resolvesToImdsIp(resolvedIps, "example.com"));
+    }
+
+    @Test
+    void testResolvesToImdsIp_WithMultipleResolvedIps_OneImdsIp() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("192.168.1.1"); // Non-IMDS IP
+        resolvedIps.add("fd00:ec2::254"); // IMDS IP
+
+        assertTrue(Resolver.resolvesToImdsIp(resolvedIps, "example.com"));
+    }
+
+    @Test
+    void testResolvesToImdsIp_WithNoImdsIp() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("192.168.1.1"); // Non-IMDS IP
+        resolvedIps.add("10.0.0.1"); // Another Non-IMDS IP
+
+        assertFalse(Resolver.resolvesToImdsIp(resolvedIps, "example.com"));
+    }
+
+    @Test
+    void testResolvesToImdsIp_WithMultipleResolvedIps_NoImdsIp() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("192.168.1.1"); // Non-IMDS IP
+        resolvedIps.add("10.0.0.1"); // Another Non-IMDS IP
+
+        assertFalse(Resolver.resolvesToImdsIp(resolvedIps, "example.com"));
+    }
+
+    @Test
+    void testResolvesToImdsIp_WithMultipleResolvedIps_OnlyTrustedHostname() {
+        Set<String> resolvedIps = new HashSet<>();
+        resolvedIps.add("169.254.169.254"); // IMDS IP
+
+        assertFalse(Resolver.resolvesToImdsIp(resolvedIps, "metadata.google.internal"));
+    }
+}
