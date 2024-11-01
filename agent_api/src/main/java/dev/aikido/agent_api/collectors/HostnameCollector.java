9fbb937d17db37ab05f69b76d054870d65d3e676
Wout Feys <wout@aikido.dev>
2024-11-01 11:46:15 +0100

    HostnameCollector run vuln code

--- agent_api/src/main/java/dev/aikido/agent_api/collectors/HostnameCollector.java
+++ agent_api/src/main/java/dev/aikido/agent_api/collectors/HostnameCollector.java
@@ -1,13 +1,24 @@
 package dev.aikido.agent_api.collectors;
 
+import com.google.gson.Gson;
+import dev.aikido.agent_api.background.ipc_commands.AttackCommand;
+import dev.aikido.agent_api.background.utilities.IPCClient;
+import dev.aikido.agent_api.background.utilities.IPCDefaultClient;
+import dev.aikido.agent_api.context.Context;
 import dev.aikido.agent_api.storage.Hostnames;
 import dev.aikido.agent_api.thread_cache.ThreadCache;
+import dev.aikido.agent_api.vulnerabilities.Attack;
+import dev.aikido.agent_api.vulnerabilities.Scanner;
+import dev.aikido.agent_api.vulnerabilities.ssrf.SSRFDetector;
+import dev.aikido.agent_api.vulnerabilities.ssrf.SSRFException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
+
+import static dev.aikido.agent_api.helpers.ShouldBlockHelper.shouldBlock;
 
 public class HostnameCollector {
     private static final Logger logger = LogManager.getLogger(HostnameCollector.class);
@@ -23,6 +34,27 @@ public class HostnameCollector {
                 continue;
             }
             logger.debug("Hostname: {}, Port: {}, IPs: {}", hostnameEntry.getHostname(), hostnameEntry.getPort(), ipAddresses);
+
+            Attack attack = new SSRFDetector().run(
+                    hostname, hostnameEntry.getPort(), ipAddresses,
+                    /* operation: */ "java.net.InetAddress.getAllByName");
+            if(attack == null) {
+                continue;
+            }
+
+            Gson gson = new Gson();
+            String json = gson.toJson(new AttackCommand.AttackCommandData(attack, Context.get()));
+
+            IPCClient client = new IPCDefaultClient();
+            client.sendData(
+                    "ATTACK$" + json, // data
+                    false // receive
+            );
+            logger.info("Sent attack data");
+            if(shouldBlock()) {
+                logger.debug("Blocking SSRF attack...");
+                throw SSRFException.get();
+            }
         }
     }
 }
