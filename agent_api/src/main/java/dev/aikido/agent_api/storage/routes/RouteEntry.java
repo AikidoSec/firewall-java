6879dda5e6ddbab8f72de65ebdd982acc1063d63
Wout Feys <wout@aikido.dev>
2024-11-07 15:19:12 +0100

    Add apispec to route entry

--- agent_api/src/main/java/dev/aikido/agent_api/storage/routes/RouteEntry.java
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/routes/RouteEntry.java
@@ -1,16 +1,19 @@
 package dev.aikido.agent_api.storage.routes;
 
+import dev.aikido.agent_api.api_discovery.APISpec;
 import dev.aikido.agent_api.context.RouteMetadata;
 
 public class RouteEntry {
     private final String method;
     private final String path;
     private int hits;
+    private APISpec apispec;
 
     public RouteEntry(String method, String path) {
         this.method = method;
         this.path = path;
         this.hits = 0;
+        this.apispec = null;
     }
     public RouteEntry(RouteMetadata routeMetadata) {
         this(routeMetadata.method(), routeMetadata.route());
@@ -30,5 +33,12 @@ public class RouteEntry {
 
     public String getPath() {
         return path;
+    }
+
+    public APISpec updateApiSpec(APISpec newApiSpec) {
+        this.apispec = newApiSpec;
+    }
+    public APISpec getApispec() {
+        return apispec;
     }
 }
