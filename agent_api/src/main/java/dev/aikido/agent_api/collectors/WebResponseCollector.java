142bcb75ad92db057ac7e8cc05f1d1939d534b7b
Wout Feys <wout@aikido.dev>
2024-11-07 15:19:38 +0100

    Update WebResponseController to allow for api discovery (WIP)

--- agent_api/src/main/java/dev/aikido/agent_api/collectors/WebResponseCollector.java
+++ agent_api/src/main/java/dev/aikido/agent_api/collectors/WebResponseCollector.java
@@ -4,10 +4,16 @@ import com.google.gson.Gson;
 import dev.aikido.agent_api.background.utilities.IPCDefaultClient;
 import dev.aikido.agent_api.context.Context;
 import dev.aikido.agent_api.context.ContextObject;
+import dev.aikido.agent_api.context.RouteMetadata;
+import dev.aikido.agent_api.storage.routes.RouteEntry;
+import dev.aikido.agent_api.thread_cache.ThreadCache;
+import dev.aikido.agent_api.thread_cache.ThreadCacheObject;
 
 import static dev.aikido.agent_api.helpers.url.IsUsefulRoute.isUsefulRoute;
 
 public class WebResponseCollector {
+    // Only do API Discovery on first 20 requests:
+    private static final int ANALYSIS_ON_FIRST_X_REQUESTS = 20;
     /**
      * This function gets used after the code of a request is complete, and we have a status code
      * Here we can check if a route is useful, report it, build api specs, ...
@@ -18,11 +24,25 @@ public class WebResponseCollector {
             return; // Status code below or equal to zero: Invalid request
         }
         ContextObject context = Context.get();
-        boolean currentRouteUseful = isUsefulRoute(statusCode, context.getRoute(), context.getMethod());
-        if (currentRouteUseful) {
-            Gson gson = new Gson();
-            String data = "INIT_ROUTE$" + gson.toJson(context.getRouteMetadata());
-            new IPCDefaultClient().sendData(data, false /* does not receive a response*/);
+        RouteMetadata routeMetadata = context.getRouteMetadata();
+        if (routeMetadata == null || !isUsefulRoute(statusCode, context.getRoute(), context.getMethod())) {
+            return;
+        }
+        // Report route :
+        Gson gson = new Gson();
+        String data = "INIT_ROUTE$" + gson.toJson(routeMetadata);
+        new IPCDefaultClient().sendData(data, false /* does not receive a response*/);
+
+        // API Spec code :
+        ThreadCacheObject threadCache = ThreadCache.get();
+        if (threadCache != null && threadCache.getRoutes() != null) {
+            threadCache.getRoutes().initializeRoute(routeMetadata); // Make sure route exists
+            RouteEntry route = threadCache.getRoutes().get(routeMetadata);
+            route.incrementHits(); // Increment hits so we can limit with constant:
+            if (route.getHits() <= ANALYSIS_ON_FIRST_X_REQUESTS) {
+                String apiDiscoveryStr = "API_DISCOVERY$" + gson.toJson(routeMetadata);
+                new IPCDefaultClient().sendData(apiDiscoveryStr, false /* does not receive a response*/);
+            }
         }
     }
 }
