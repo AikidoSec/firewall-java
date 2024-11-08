340f29446529f022812b7a976aed5ed22c0ee84d
Wout Feys <wout@aikido.dev>
2024-11-08 14:23:51 +0100

    Create an API Spec merger

---
+++ agent_api/src/main/java/dev/aikido/agent_api/api_discovery/APISpecMerger.java
@@ -0,0 +1,39 @@
+package dev.aikido.agent_api.api_discovery;
+
+import java.util.List;
+import java.util.Map;
+
+import static dev.aikido.agent_api.api_discovery.AuthTypeMerger.mergeAuthTypes;
+import static dev.aikido.agent_api.api_discovery.DataSchemaMerger.mergeDataSchemas;
+
+public class APISpecMerger {
+    public static APISpec mergeAPISpecs(APISpec updatedApiSpec, APISpec oldApiSpec) {
+        if (updatedApiSpec == null) {
+            return oldApiSpec;
+        }
+        if(oldApiSpec == null) {
+            return updatedApiSpec;
+        }
+
+        // Body :
+        APISpec.Body body = oldApiSpec.body();
+        if (updatedApiSpec.body() != null && oldApiSpec.body() != null) {
+            DataSchemaItem schema = mergeDataSchemas(oldApiSpec.body().schema(), updatedApiSpec.body().schema());
+            body = new APISpec.Body(/* schema: */ schema, /* type: */ updatedApiSpec.body().type());
+        } else if (updatedApiSpec.body() != null) {
+            body = updatedApiSpec.body();
+        }
+
+        // Query :
+        DataSchemaItem query = oldApiSpec.query();
+        if (oldApiSpec.query() != null && updatedApiSpec.query() != null) {
+            query = mergeDataSchemas(oldApiSpec.query(), updatedApiSpec.query());
+        } else if(updatedApiSpec.query() != null) {
+            query = updatedApiSpec.query();
+        }
+
+        // Auth :
+        List<Map<String, String>> auth = mergeAuthTypes(oldApiSpec.auth(), updatedApiSpec.auth());
+        return new APISpec(body, query, auth);
+    }
+}
