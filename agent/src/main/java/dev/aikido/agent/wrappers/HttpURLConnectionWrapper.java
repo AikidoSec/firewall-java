e32a38e470fb3c9f3f444510174d6d6681be289f
Wout Feys <wout@aikido.dev>
2024-11-19 13:40:00 +0100

    Wrap constructor of HttpURLConnection instead of getResponseCode

--- agent/src/main/java/dev/aikido/agent/wrappers/HttpURLConnectionWrapper.java
+++ agent/src/main/java/dev/aikido/agent/wrappers/HttpURLConnectionWrapper.java
@@ -6,6 +6,7 @@ import net.bytebuddy.matcher.ElementMatchers;
 
 import java.io.IOException;
 import java.lang.reflect.Executable;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.*;
@@ -14,21 +15,21 @@ import static net.bytebuddy.implementation.bytecode.assign.Assigner.Typing.DYNAM
 
 public class HttpURLConnectionWrapper implements Wrapper {
     public String getName() {
-        // Wrap getResponseCode function which executes HTTP requests
-        // https://docs.oracle.com/javase/8/docs/api/java/net/HttpURLConnection.html#getResponseCode--
-        return GetResponseAdvice.class.getName();
+        // Wrap Constructor of HttpURLConnection
+        // https://docs.oracle.com/javase/8/docs/api/java/net/HttpURLConnection.html
+        return ConstructorAdvice.class.getName();
     }
     public ElementMatcher<? super MethodDescription> getMatcher() {
-        return ElementMatchers.nameContainsIgnoreCase("getResponseCode");
+        return ElementMatchers.isDeclaredBy(ElementMatchers.nameContainsIgnoreCase("java.net.HttpURLConnection"))
+                .and(ElementMatchers.isConstructor());
     }
-    public static class GetResponseAdvice {
+    public static class ConstructorAdvice {
         // Since we have to wrap a native Java Class stuff gets more complicated
         // The classpath is not the same anymore, and we can't import our modules directly.
         // To bypass this issue we load collectors from a .jar file, specified with the AIKIDO_DIRECTORY env variable
-        @Advice.OnMethodEnter
-        public static void before(
-                @Advice.This(typing = DYNAMIC, optional = true) HttpURLConnection target,
-                @Advice.Origin Executable method
+        @Advice.OnMethodExit
+        public static void after(
+                @Advice.This(typing = DYNAMIC, optional = true) HttpURLConnection target
         ) {
             String pathToAikidoFolder = System.getenv("AIKIDO_DIRECTORY");
             String jarFilePath = "file:" + pathToAikidoFolder + "agent_api.jar";
