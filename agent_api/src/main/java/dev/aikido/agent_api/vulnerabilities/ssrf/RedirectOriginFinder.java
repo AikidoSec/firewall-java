6d3b7adb08035d0c3385f1fb8934311266349bdb
Wout Feys <wout@aikido.dev>
2024-11-20 17:16:05 +0100

    Create a W.I.P. RedirectOriginFinder

---
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/RedirectOriginFinder.java
@@ -0,0 +1,29 @@
+package dev.aikido.agent_api.vulnerabilities.ssrf;
+
+import dev.aikido.agent_api.context.Context;
+import dev.aikido.agent_api.storage.RedirectNode;
+
+import java.net.URL;
+import java.util.List;
+
+public class RedirectOriginFinder {
+    public URL getRedirectOrigin(String hostname, int port) {
+        List<RedirectNode> redirectStartNodes = Context.get().getRedirectStartNodes();
+
+        // Loop over all start nodes :
+        for (RedirectNode node: redirectStartNodes) {
+            RedirectNode currentChild = node.getChild();
+            while (currentChild != null) {
+                if (currentChild.getUrl().getPort() == port &&
+                    currentChild.getUrl().getHost().equals(hostname)) {
+                    // Check if this child has the same Hostname and port as provided
+                    return node.getUrl();
+                }
+
+                // Traverse chain downwards :
+                currentChild = currentChild.getChild();
+            }
+        }
+        return null;
+    }
+}
