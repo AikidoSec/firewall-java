8c26857f45266e1906b4e339389e748d7739e937
Wout Feys <wout@aikido.dev>
2024-11-21 21:03:27 +0100

    Create one JDBCConnectionAdvice class

---
+++ agent/src/main/java/dev/aikido/agent/wrappers/jdbc/JDBCConnectionAdvice.java
@@ -0,0 +1,45 @@
+package dev.aikido.agent.wrappers.jdbc;
+
+import dev.aikido.agent_api.collectors.SQLCollector;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.reflect.Executable;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+import static net.bytebuddy.implementation.bytecode.assign.Assigner.Typing.DYNAMIC;
+
+public final class JDBCConnectionAdvice {
+    private JDBCConnectionAdvice() {}
+    public static ElementMatcher<? super MethodDescription> getMatcher(String module) {
+        return ElementMatchers.isDeclaredBy(ElementMatchers.nameContainsIgnoreCase(module)).and(
+                ElementMatchers.named("prepareStatement")
+                        .or(ElementMatchers.named("prepareCall"))
+                        .or(ElementMatchers.named("nativeSQL"))
+        );
+    }
+
+    /* Wraps all functions accepting sql for the Connection interface :
+     * -> prepareStatement(sql, [...]), prepareCall(sql, [...]), nativeSQL(sql)
+     * https://docs.oracle.com/javase/8/docs/api/java/sql/Connection.html#nativeSQL-java.lang.String-
+     */
+    @Advice.OnMethodEnter
+    public static void before(
+            @Advice.This(typing = DYNAMIC, optional = true) Connection connection,
+            @Advice.Origin Executable method,
+            @Advice.Argument(0) String sql
+    ) {
+        if (sql != null) {
+            try {
+                DatabaseMetaData metaData = connection.getMetaData();
+                String operation = "(" + metaData.getDriverName() + ") java.sql.Connection." + method.getName();
+                String dialect = metaData.getDatabaseProductName().toLowerCase();
+                SQLCollector.report(sql, dialect, operation);
+
+            } catch (Throwable ignored) {}
+        }
+    }
+}
