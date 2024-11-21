80ea6c902d59b799728e7c9260865d008c72606c
Wout Feys <wout@aikido.dev>
2024-11-21 10:09:49 +0100

    Add unit tests to redirect origin finder

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/RedirectOriginFinder.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/RedirectOriginFinder.java
@@ -6,15 +6,18 @@ import dev.aikido.agent_api.storage.RedirectNode;
 import java.net.URL;
 import java.util.List;
 
+import static dev.aikido.agent_api.helpers.url.PortParser.getPortFromURL;
+
 public class RedirectOriginFinder {
-    public URL getRedirectOrigin(String hostname, int port) {
+    public static URL getRedirectOrigin(String hostname, int port) {
         List<RedirectNode> redirectStartNodes = Context.get().getRedirectStartNodes();
 
         // Loop over all start nodes :
         for (RedirectNode node: redirectStartNodes) {
             RedirectNode currentChild = node.getChild();
             while (currentChild != null) {
-                if (currentChild.getUrl().getPort() == port &&
+                int childPort = getPortFromURL(currentChild.getUrl());
+                if (childPort == port &&
                     currentChild.getUrl().getHost().equals(hostname)) {
                     // Check if this child has the same Hostname and port as provided
                     return node.getUrl();
