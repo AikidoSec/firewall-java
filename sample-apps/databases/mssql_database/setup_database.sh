5a5844146cd90145b6e3447fab7db4ea8224a414
Wout Feys <wout@aikido.dev>
2024-12-02 14:39:24 +0100

    Sleep+ for MSSQL

--- sample-apps/databases/mssql_database/setup_database.sh
+++ sample-apps/databases/mssql_database/setup_database.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Wait for database to startup 
-sleep 15
+sleep 20
+echo "Setup of database"
 ./opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "Strong!Passw0rd" -i setup.sql -N -C
