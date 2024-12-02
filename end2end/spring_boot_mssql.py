8f092d66089ff3da9fcd4d618ec4444696fa7755
Wout Feys <wout@aikido.dev>
2024-12-02 13:00:09 +0100

    Add response text spring boot mssql

--- end2end/spring_boot_mssql.py
+++ end2end/spring_boot_mssql.py
@@ -18,7 +18,7 @@ def make_post_request(url, data, status_code):
     response = requests.post(url, json=data)
 
     # Assert that the status code is 200
-    assert response.status_code == status_code, f"Expected status code {status_code} but got {response.status_code}"
+    assert response.status_code == status_code, f"Expected status code {status_code} but got {response.status_code} With text: {response.text}"
 
     # If you want to check the response content, you can do so here
     print(f"Success: {response.json()}")
