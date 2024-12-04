9eeb0d4ed3675699130acd25978a41d07211e424
Wout Feys <wout@aikido.dev>
2024-12-04 08:54:37 +0100

    Add sink tests for Runtime and only check java.lang.Runtime not java.lang

---
+++ agent_api/src/test/java/wrappers/RuntimeExecTest.java
@@ -0,0 +1,78 @@
+package wrappers;
+
+import dev.aikido.agent_api.context.Context;
+import dev.aikido.agent_api.context.ContextObject;
+import dev.aikido.agent_api.storage.routes.Routes;
+import dev.aikido.agent_api.thread_cache.ThreadCache;
+import dev.aikido.agent_api.thread_cache.ThreadCacheObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+import java.io.File;
+import java.net.http.HttpClient;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RuntimeExecTest {
+    public static class SampleContextObject extends ContextObject {
+        public SampleContextObject(String argument) {
+            this.method = "GET";
+            this.source = "web";
+            this.url = "https://example.com/api/resource";
+            this.route = "/api/resource";
+            this.remoteAddress = "192.168.1.1";
+            this.headers = new HashMap<>();
+
+            this.query = new HashMap<>();
+            this.query.put("search", new String[]{"example", "dev.aikido:80"});
+            this.query.put("sql1", new String[]{"SELECT * FRO"});
+            this.query.put("arg", new String[]{argument});
+
+            this.cookies = new HashMap<>();
+            this.body = "{\"key\":\"value\"}"; // Body as a JSON string
+        }
+    }
+
+    @AfterEach
+    void cleanup() {
+        Context.set(null);
+        ThreadCache.set(null);
+    }
+    @BeforeEach
+    void clearThreadCache() {
+        cleanup();
+        ThreadCache.set(new ThreadCacheObject(List.of(), Set.of(), Set.of(), new Routes()));
+    }
+    private void setContextAndLifecycle(String url) {
+        Context.set(new SampleContextObject(url));
+    }
+
+    @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "invalid-token-2")
+    @SetEnvironmentVariable(key = "AIKIDO_BLOCKING", value = "true")
+    @Test
+    public void testShellInjection() throws Exception {
+        setContextAndLifecycle(" -la");
+        Exception exception1 = assertThrows(RuntimeException.class, () -> {
+            Runtime.getRuntime().exec("ls -la");
+        });
+        assertEquals("Aikido Zen has blocked Shell Injection", exception1.getMessage());
+
+        cleanup();
+        setContextAndLifecycle("whoami");
+        Exception exception2 = assertThrows(RuntimeException.class, () -> {
+            Runtime.getRuntime().exec("whoami");
+        });
+        assertEquals("Aikido Zen has blocked Shell Injection", exception2.getMessage());
+
+
+        cleanup();
+        assertDoesNotThrow(() -> {
+            Runtime.getRuntime().exec("whoami && ls -la");
+        });
+    }
+}
