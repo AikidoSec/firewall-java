e5b0aed6590083ac70f04ab54f4d2c78984701dd
Wout Feys <wout@aikido.dev>
2024-12-12 10:27:02 +0100

    Just disable some supposedly slow wrappers

--- agent/src/main/java/dev/aikido/agent/Wrappers.java
+++ agent/src/main/java/dev/aikido/agent/Wrappers.java
@@ -12,25 +12,23 @@ import java.util.List;
 public final class Wrappers {
     private Wrappers() {}
     public static final List<Wrapper> WRAPPERS = Arrays.asList(
-            new PostgresWrapper()
-            /*
-            new SpringFrameworkWrapper(),
-            new SpringFrameworkBodyWrapper(),
+            new PostgresWrapper(),
+            //new SpringFrameworkWrapper(),
+            //new SpringFrameworkBodyWrapper(),
             new FileWrapper(),
             new URLConnectionWrapper(),
             new InetAddressWrapper(),
             new RuntimeExecWrapper(),
-            new SpringFrameworkInvokeWrapper(),
+            //new SpringFrameworkInvokeWrapper(),
             new MysqlCJWrapper(),
             new MSSQLWrapper(),
             new MariaDBWrapper(),
             new HttpClientWrapper(),
             new HttpConnectionRedirectWrapper(),
             new HttpClientSendWrapper(),
-            new OkHttpWrapper(),
-            new ApacheHttpClientWrapper(),
+            //new OkHttpWrapper(),
+            //new ApacheHttpClientWrapper(),
             new PathWrapper(),
             new PathsWrapper()
-             */
     );
 }
