a2b15534442e0e887b1c6f79ecaea84f798bf446
Wout Feys <wout@aikido.dev>
2024-12-16 16:53:20 +0100

    Update all commands to follow new abstract class

--- agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/BlockingEnabledCommand.java
+++ agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/BlockingEnabledCommand.java
@@ -1,12 +1,11 @@
 package dev.aikido.agent_api.background.ipc_commands;
 
-import com.google.gson.Gson;
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
 
 import java.util.Optional;
 
-public class BlockingEnabledCommand implements  Command {
-    public record BlockingEnabledResult(boolean isBlockingEnabled) {}
+public class BlockingEnabledCommand extends Command<Command.EmptyResult, BlockingEnabledCommand.Res> {
+    public record Res(boolean isBlockingEnabled) {}
 
     @Override
     public boolean returnsData() {
@@ -15,17 +14,12 @@ public class BlockingEnabledCommand implements  Command {
     }
 
     @Override
-    public boolean matchesName(String command) {
-        return command.equalsIgnoreCase("BLOCKING_ENABLED");
-    }
+    public String getName() { return "BLOCKING_ENABLED"; }
 
     @Override
-    public Optional<String> execute(String data, CloudConnectionManager connectionManager) {
+    public Optional<Res> execute(EmptyResult data, CloudConnectionManager connectionManager) {
         // "data" can be safely ignored, we just need to return if blocking is enabled
         boolean isBlockingEnabled = connectionManager.shouldBlock();
-
-        Gson gson = new Gson();
-        String result = gson.toJson(new BlockingEnabledResult(isBlockingEnabled));
-        return Optional.of(result);
+        return Optional.of( new Res(isBlockingEnabled));
     }
 }
--- agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/InitRouteCommand.java
+++ agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/InitRouteCommand.java
@@ -1,31 +1,28 @@
 package dev.aikido.agent_api.background.ipc_commands;
 
-import com.google.gson.Gson;
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
 import dev.aikido.agent_api.context.RouteMetadata;
 
 import java.util.Optional;
 
-public class InitRouteCommand implements Command {
+public class InitRouteCommand extends Command<RouteMetadata, Command.EmptyResult> {
     @Override
     public boolean returnsData() {
         return false;
     }
 
     @Override
-    public boolean matchesName(String command) {
-        return command.equalsIgnoreCase("INIT_ROUTE");
+    public String getName() {
+        return "INIT_ROUTE";
     }
 
     /**
      *
-     * @param data is JSON String for {@code RouteMetadata}
+     * @param routeMetadata of class RouteMetadata
      * @return Nothing (Empty optional)
      */
     @Override
-    public Optional<String> execute(String data, CloudConnectionManager connectionManager) {
-        Gson gson = new Gson();
-        RouteMetadata routeMetadata = gson.fromJson(data, RouteMetadata.class);
+    public Optional<EmptyResult> execute(RouteMetadata routeMetadata, CloudConnectionManager connectionManager) {
         if (connectionManager.getRoutes().get(routeMetadata) == null) {
             connectionManager.getRoutes().initializeRoute(routeMetadata);
         }
