138270f8283d32dbb75d45f470304060cd81abd4
Wout Feys <wout@aikido.dev>
2024-12-16 12:58:41 +0100

    Write unit tests for ShouldBlockHelper

---
+++ agent_api/src/test/java/helpers/ShouldBlockHelperTest.java
@@ -0,0 +1,25 @@
+package helpers;
+
+import dev.aikido.agent_api.helpers.ShouldBlockHelper;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShouldBlockHelperTest {
+
+    @Test
+    @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "")
+    @SetEnvironmentVariable(key = "AIKIDO_BLOCKING", value = "true")
+    public void testWithInvalidAikidoTokenTrue() {
+        assertTrue(ShouldBlockHelper.shouldBlock());
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "")
+    @SetEnvironmentVariable(key = "AIKIDO_BLOCKING", value = "false")
+    public void testWithInvalidAikidoTokenFalse() {
+        assertFalse(ShouldBlockHelper.shouldBlock());
+    }
+}
