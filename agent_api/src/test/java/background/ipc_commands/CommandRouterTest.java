d187d4c33f7e99c8ba273ff33849a9fcafee284e
Wout Feys <wout@aikido.dev>
2024-12-16 18:37:56 +0100

    Fix Unit tests

--- agent_api/src/test/java/background/ipc_commands/CommandRouterTest.java
+++ agent_api/src/test/java/background/ipc_commands/CommandRouterTest.java
@@ -1,11 +1,15 @@
 package background.ipc_commands;
 
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
+import dev.aikido.agent_api.background.ipc_commands.BlockingEnabledCommand;
 import dev.aikido.agent_api.background.ipc_commands.CommandRouter;
+import dev.aikido.agent_api.helpers.Serializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -27,26 +31,26 @@ public class CommandRouterTest {
     }
     @Test
     public void testIPCInputIsMalformed() {
-        Optional<String> result = commandRouter.parseIPCInput("BLOCKING_ENABLED%{}");
+        Optional<byte[]> result = commandRouter.parseIPCInput("BLOCKING_ENABLED%{}".getBytes(StandardCharsets.UTF_8));
         assertTrue(result.isEmpty());
     }
 
     @Test
     public void testNonExistingCommand() {
-        Optional<String> result = commandRouter.parseIPCInput("INVALID_COMMAND${}");
+        Optional<byte[]> result = commandRouter.parseIPCInput("INVALID_COMMAND%{}".getBytes(StandardCharsets.UTF_8));
         assertTrue(result.isEmpty());
     }
 
     @Test
-    public void testShouldBlockCommand() {
+    public void testShouldBlockCommand() throws IOException {
         when(cloudConnectionManager.shouldBlock()).thenReturn(true);
-        Optional<String> result = commandRouter.parseIPCInput("BLOCKING_ENABLED${}");
+        Optional<byte[]> result = commandRouter.parseIPCInput("BLOCKING_ENABLED%{}".getBytes(StandardCharsets.UTF_8));
         assertTrue(result.isPresent());
-        assertEquals("{\"isBlockingEnabled\":true}", result.get());
+        assertEquals(Serializer.serializeData(new BlockingEnabledCommand.Res(true)), result.get());
 
         when(cloudConnectionManager.shouldBlock()).thenReturn(false);
-        Optional<String> result2 = commandRouter.parseIPCInput("BLOCKING_ENABLED${}");
+        Optional<byte[]> result2 = commandRouter.parseIPCInput("BLOCKING_ENABLED%{}".getBytes(StandardCharsets.UTF_8));
         assertTrue(result2.isPresent());
-        assertEquals("{\"isBlockingEnabled\":false}", result2.get());
+        assertEquals(Serializer.serializeData(new BlockingEnabledCommand.Res(false)), result2.get());
     }
 }
