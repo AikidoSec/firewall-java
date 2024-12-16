b941efba0e9223b459f64021757ade4eacd17f09
Wout Feys <wout@aikido.dev>
2024-12-16 18:16:24 +0100

    Command now also serializes and creates a byte array with all data

--- agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/Command.java
+++ agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/Command.java
@@ -2,17 +2,26 @@ package dev.aikido.agent_api.background.ipc_commands;
 
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
 import dev.aikido.agent_api.background.utilities.ThreadClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.text.html.Option;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+
+import static dev.aikido.agent_api.helpers.Serializer.deserializeData;
+import static dev.aikido.agent_api.helpers.Serializer.serializeData;
+import static dev.aikido.agent_api.helpers.extraction.ByteArrayHelper.joinByteArrays;
 
 /**
  * Command interface for IPC Commands
  * E.g. AttackCommand : gives false in returnsData(), matches it's name for "ATTACK" and executes code to report attacks.
  */
 public abstract class Command<I, O> {
-    public record EmptyResult() {}
+    private static final Logger logger = LogManager.getLogger(Command.class);
+
+    public record EmptyResult() implements Serializable {}
     public abstract boolean returnsData();
     public abstract String getName();
 
@@ -23,33 +32,27 @@ public abstract class Command<I, O> {
     public abstract Optional<O> execute(I data, CloudConnectionManager connectionManager);
 
     public Optional<O> send(ThreadClient threadClient, I input) {
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
-            oos.writeObject(input);
-            byte[] inputAsBytes = bos.toByteArray();
-            Optional<byte[]> response = threadClient.send(inputAsBytes, returnsData());
+        logger.debug("[--> Sending over IPC: {}", input);
+        try {
+            byte[] inputAsBytes = serializeData(input);
+            byte[] identifier = (getName() + "$").getBytes(StandardCharsets.UTF_8);
+            Optional<byte[]> response = threadClient.send(joinByteArrays(identifier, inputAsBytes), returnsData());
             if(!response.isEmpty()) {
-                return Optional.of(deserializeOutput(response.get()));
+                O data = deserializeData(response.get());
+                logger.debug("[<-- Data returned: {}", data);
+                return Optional.of(data);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.trace(e);
         }
         return Optional.empty();
     };
 
     public I deserializeInput(byte[] input) {
-        try (ByteArrayInputStream bis = new ByteArrayInputStream(input);
-             ObjectInputStream ois = new ObjectInputStream(bis)) {
-            return (I) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            return null;
-        }
-    };
-    private O deserializeOutput(byte[] input) {
-        try (ByteArrayInputStream bis = new ByteArrayInputStream(input);
-             ObjectInputStream ois = new ObjectInputStream(bis)) {
-            return (O) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+        try {
+            return deserializeData(input);
+        } catch (Exception e) {
+            logger.trace(e);
             return null;
         }
     };
