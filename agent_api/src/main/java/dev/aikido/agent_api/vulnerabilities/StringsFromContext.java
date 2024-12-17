9c1a565640c7d193c895b13d14b2f7264ce07e8b
Wout Feys <wout@aikido.dev>
2024-12-17 10:35:29 +0100

    Add a cache for extracted strings

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/StringsFromContext.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/StringsFromContext.java
@@ -12,11 +12,41 @@ public class StringsFromContext {
     private final Map<String, String> cookieStrings;
     private final Map<String, String> routeParamStrings;
     public StringsFromContext(ContextObject contextObject) {
-        bodyStrings = StringExtractor.extractStringsFromObject(contextObject.getBody());
-        queryStrings = StringExtractor.extractStringsFromObject(contextObject.getQuery());
-        headersStrings = StringExtractor.extractStringsFromObject(contextObject.getHeaders());
-        cookieStrings = StringExtractor.extractStringsFromObject(contextObject.getCookies());
-        routeParamStrings = StringExtractor.extractStringsFromObject(contextObject.getParams());
+        // Body :
+        if (!contextObject.getCache().containsKey("body")) {
+            bodyStrings = StringExtractor.extractStringsFromObject(contextObject.getBody());
+            contextObject.getCache().put("body", bodyStrings);
+        } else {
+            bodyStrings = contextObject.getCache().get("body");
+        }
+        // Query :
+        if (!contextObject.getCache().containsKey("query")) {
+            queryStrings = StringExtractor.extractStringsFromObject(contextObject.getQuery());
+            contextObject.getCache().put("query", queryStrings);
+        } else {
+            queryStrings = contextObject.getCache().get("query");
+        }
+        // Headers :
+        if (!contextObject.getCache().containsKey("headers")) {
+            headersStrings = StringExtractor.extractStringsFromObject(contextObject.getHeaders());
+            contextObject.getCache().put("headers", headersStrings);
+        } else {
+            headersStrings = contextObject.getCache().get("headers");
+        }
+        // Cookies :
+        if (!contextObject.getCache().containsKey("cookies")) {
+            cookieStrings = StringExtractor.extractStringsFromObject(contextObject.getCookies());
+            contextObject.getCache().put("cookies", cookieStrings);
+        } else {
+            cookieStrings = contextObject.getCache().get("cookies");
+        }
+        // route parameters :
+        if (!contextObject.getCache().containsKey("routeParams")) {
+            routeParamStrings = StringExtractor.extractStringsFromObject(contextObject.getParams());
+            contextObject.getCache().put("routeParams", routeParamStrings);
+        } else {
+            routeParamStrings = contextObject.getCache().get("routeParams");
+        }
     }
     public Map<String, Map<String, String>> getAll() {
         return Map.of(
