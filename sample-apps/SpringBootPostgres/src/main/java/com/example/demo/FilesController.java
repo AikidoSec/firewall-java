d2b6ff8849b250a1c0b935db0b70748c92817c0c
Wout Feys <wout@aikido.dev>
2024-12-20 14:12:15 +0100

    Update sample apps so we can test more

--- sample-apps/SpringBootPostgres/src/main/java/com/example/demo/FilesController.java
+++ sample-apps/SpringBootPostgres/src/main/java/com/example/demo/FilesController.java
@@ -10,12 +10,10 @@ import java.util.Scanner;
 @RestController
 @RequestMapping("/api/files") // Base URL for all routes in this controller
 public class FilesController {
-    private record FileRead(String fileName) {}
     @PostMapping(path = "/read",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public String read(@RequestBody FileRead fileData) {
-        String fileName = fileData.fileName;
+    public String read(@RequestParam String fileName) {
         System.out.println("Reading file: "+ fileName);
         File specifiedFile = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
