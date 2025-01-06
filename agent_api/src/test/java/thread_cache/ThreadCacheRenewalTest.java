9acb7c044adec4ece74549b4a0c82f4a776af111
Wout Feys <wout@aikido.dev>
2025-01-06 13:53:15 +0100

    Set temp dir again, now with explainer

--- agent_api/src/test/java/thread_cache/ThreadCacheRenewalTest.java
+++ agent_api/src/test/java/thread_cache/ThreadCacheRenewalTest.java
@@ -22,6 +22,8 @@ public class ThreadCacheRenewalTest {
 
     @Test
     @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "token-123456")
+    // Set AIKIDO_TMP_DIR to /opt/aikido, this is what gets created for Github workflows
+    @SetEnvironmentVariable(key = "AIKIDO_TMP_DIR", value = "/opt/aikido")
     public void renewWithEmptyBackgroundProcess() throws InterruptedException {
         BackgroundProcess backgroundProcess = new BackgroundProcess("test-background-process", new Token("token-123456"));
         backgroundProcess.setDaemon(true);
