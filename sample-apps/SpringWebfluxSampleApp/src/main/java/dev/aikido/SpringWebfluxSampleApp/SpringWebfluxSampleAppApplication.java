6c2138c2de01a71597a97c053ede0eec29e4d276
Wout Feys <wout@aikido.dev>
2025-01-07 12:11:51 +0100

    Add Spring Webflux sample app

---
+++ sample-apps/SpringWebfluxSampleApp/src/main/java/dev/aikido/SpringWebfluxSampleApp/SpringWebfluxSampleAppApplication.java
@@ -0,0 +1,13 @@
+package dev.aikido.SpringWebfluxSampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxSampleAppApplication.class, args);
+	}
+
+}
