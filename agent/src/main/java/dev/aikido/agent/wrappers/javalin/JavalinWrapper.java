738b19a9dbe34a33b8f1bdc9edec603506a814e4
BitterPanda63 <bitterpanda@proton.me>
2025-01-20 09:41:26 +0100

    JavalinWrapper and context + response writing

---
+++ agent/src/main/java/dev/aikido/agent/wrappers/javalin/JavalinWrapper.java
@@ -0,0 +1,70 @@
+package dev.aikido.agent.wrappers.javalin;
+
+import dev.aikido.agent.wrappers.Wrapper;
+import dev.aikido.agent_api.collectors.WebRequestCollector;
+import dev.aikido.agent_api.collectors.WebResponseCollector;
+import dev.aikido.agent_api.context.Context;
+import dev.aikido.agent_api.context.ContextObject;
+import dev.aikido.agent_api.context.JavalinContextObject;
+import io.javalin.http.servlet.JavalinServletContext;
+import jakarta.servlet.http.HttpServletResponse;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.*;
+
+import java.lang.reflect.Executable;
+
+import static net.bytebuddy.implementation.bytecode.assign.Assigner.Typing.DYNAMIC;
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+/** JavalinWrapper
+ * We're wrapping io.javalin.router.ParsedEndpoint's handle(JavalinServletContext, ...) function.
+ * See here: https://github.com/javalin/javalin/blob/8b1dc1a55c28618df7f9f044aad4949d30a8cca8/javalin/src/main/java/io/javalin/router/ParsedEndpoint.kt#L14
+ */
+public class JavalinWrapper implements Wrapper {
+    public String getName() {
+        return JavalinAdvice.class.getName();
+    }
+    public ElementMatcher<? super MethodDescription> getMatcher() {
+        return isDeclaredBy(getTypeMatcher()).and(named("handle"));
+    }
+    @Override
+    public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+        return nameContains("io.javalin.router.ParsedEndpoint");
+    }
+    public class JavalinAdvice {
+        @Advice.OnMethodEnter(suppress = Throwable.class)
+        public static JavalinServletContext before(
+                @Advice.This(typing = DYNAMIC, optional = true) Object target,
+                @Advice.Origin Executable method,
+                @Advice.Argument(value = 0, typing = DYNAMIC, optional = true) JavalinServletContext ctx
+                ) {
+            if (Context.get() != null) {
+                return ctx; // Do not extract if context already exists.
+            }
+            // Create a context object :
+            ContextObject context = new JavalinContextObject(
+                    ctx.method().name(), ctx.url(), ctx.ip(),
+                    ctx.queryParamMap(), ctx.cookieMap(), ctx.headerMap()
+            );
+            WebRequestCollector.Res response = WebRequestCollector.report(context);
+
+            // Write a response if necessary :
+            if (response != null) {
+                ctx.result(response.msg());
+                ctx.status(response.status());
+                ctx.skipRemainingHandlers();
+            }
+
+            return ctx;
+        }
+        @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+        public static void after(
+                @Advice.Enter(typing = DYNAMIC) JavalinServletContext ctx
+        ) {
+            WebResponseCollector.report(ctx.statusCode());
+        }
+    }
+
+}
---
+++ agent_api/src/main/java/dev/aikido/agent_api/context/JavalinContextObject.java
@@ -0,0 +1,47 @@
+package dev.aikido.agent_api.context;
+
+import java.util.*;
+
+import static dev.aikido.agent_api.helpers.net.ProxyForwardedParser.getIpFromRequest;
+import static dev.aikido.agent_api.helpers.url.BuildRouteFromUrl.buildRouteFromUrl;
+
+public class JavalinContextObject extends ContextObject {
+    // We use this map for when @RequestBody does not get used :
+    protected transient Map<String, Object> bodyMap = new HashMap<>();
+    public JavalinContextObject(
+            String method, String url, String rawIp, Map<String, List<String>> queryParams,
+            Map<String, String> cookies, Map<String, String> headers
+    ) {
+        this.method = method;
+        if (url != null) {
+            this.url = url.toString();
+        }
+        this.query = new HashMap<>(queryParams);
+        this.cookies = extractCookies(cookies);
+        this.headers = new HashMap<>(headers);
+        this.route = buildRouteFromUrl(this.url);
+        this.remoteAddress = getIpFromRequest(rawIp, this.headers);
+        this.source = "SpringFramework";
+        this.redirectStartNodes = new ArrayList<>();
+
+        // We don't have access yet to the route parameters, will add once we have access.
+        this.params = null;
+    }
+    @Override
+    public Object getBody() {
+        return null; // IMPLEMENT
+    }
+    public void setParams(Object params) {
+        this.params = params;
+        this.cache.remove("routeParams"); // Reset cache
+    }
+
+    private static HashMap<String, List<String>> extractCookies(Map<String, String> cookieMap) {
+        HashMap<String, List<String>> cookies = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : cookieMap.entrySet()) {
+            cookies.put(entry.getKey(), List.of(entry.getValue()));
+        }
+        return cookies;
+    }
+}
