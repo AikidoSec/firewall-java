a74781d933fdd4143bd7dcfce807d1c00d56b2e3
BitterPanda63 <bitterpanda@proton.me>
2025-01-20 11:03:04 +0100

    Remove some logging statements

--- agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/Command.java
+++ agent_api/src/main/java/dev/aikido/agent_api/background/ipc_commands/Command.java
@@ -31,7 +31,6 @@ public abstract class Command<I, O> {
     public abstract Optional<O> execute(I data, CloudConnectionManager connectionManager);
 
     public Optional<O> send(ThreadIPCClient threadClient, I input) {
-        logger.trace("Sending: %s", getName());
         try {
             // Convert input data from thread to a byte[] JSON :
             Gson gson = new Gson();
@@ -43,13 +42,11 @@ public abstract class Command<I, O> {
             if(!response.isEmpty()) {
                 // Convert background process' response from byte[] JSON to the output object :
                 O data = gson.fromJson(new String(response.get(), StandardCharsets.UTF_8), getOutputClass());
-                logger.trace("Response is %", data);
                 return Optional.of(data);
             }
         } catch (Exception e) {
             logger.trace(e);
         }
-        logger.trace("Response is empty.");
         return Optional.empty();
     };
 }
