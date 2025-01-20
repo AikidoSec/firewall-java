831e1c179c0f6e9e596c7179d909bea487302c4c
BitterPanda63 <bitterpanda@proton.me>
2025-01-20 10:15:54 +0100

    Fix Kotlin/Groovy ms failure

--- benchmarks/wrk_benchmark/spring_mvc_postgres_groovy.py
+++ benchmarks/wrk_benchmark/spring_mvc_postgres_groovy.py
@@ -6,5 +6,6 @@ run_benchmark(
     "http://localhost:8094/benchmark", # Application with Zen
     "http://localhost:8095/benchmark", # Application without Zen
     "An empty route (1ms simulated delay)",
-    percentage_limit=15, ms_limit=150
+    percentage_limit=15, ms_limit=200
 )
+
--- benchmarks/wrk_benchmark/spring_mvc_postgres_kotlin.py
+++ benchmarks/wrk_benchmark/spring_mvc_postgres_kotlin.py
@@ -6,5 +6,5 @@ run_benchmark(
     "http://localhost:8092/benchmark", # Application with Zen
     "http://localhost:8093/benchmark", # Application without Zen
     "An empty route (1ms simulated delay)",
-    percentage_limit=15, ms_limit=150
+    percentage_limit=15, ms_limit=200
 )
