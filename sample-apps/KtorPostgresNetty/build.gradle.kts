dd0541f01a889af74f1d8beef630acadd844a281
BitterPanda63 <bitterpanda@proton.me>
2025-01-21 16:00:11 +0100

    Add html files and deps

--- sample-apps/KtorPostgresNetty/build.gradle.kts
+++ sample-apps/KtorPostgresNetty/build.gradle.kts
@@ -2,16 +2,14 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.serialization)
 }
 
 group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    mainClass.set("com.example.ApplicationKt")
 }
 
 repositories {
@@ -19,11 +17,15 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.gson)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.serialization.json)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.org.postgresql.postgresql)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
