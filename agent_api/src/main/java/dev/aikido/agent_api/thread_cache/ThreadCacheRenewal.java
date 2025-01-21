24e3bb369bb929a20408202ad263f00e5276e343
BitterPanda63 <bitterpanda@proton.me>
2025-01-21 16:52:22 +0100

    Update way thread cache and thread cache renewal works

--- agent_api/src/main/java/dev/aikido/agent_api/thread_cache/ThreadCacheRenewal.java
+++ agent_api/src/main/java/dev/aikido/agent_api/thread_cache/ThreadCacheRenewal.java
@@ -10,24 +10,24 @@ import java.util.Optional;
 
 import static dev.aikido.agent_api.background.utilities.ThreadIPCClientFactory.getDefaultThreadIPCClient;
 import static dev.aikido.agent_api.helpers.BackgroundProcessIdentifier.isBackgroundProcess;
+import static dev.aikido.agent_api.thread_cache.ThreadCache.threadCache;
 
 public final class ThreadCacheRenewal {
     private ThreadCacheRenewal() {}
-    public static ThreadCacheObject renewThreadCache() {
+    public static ThreadCacheObject renewThreadCache() { return renewThreadCache(null); }
+    public static ThreadCacheObject renewThreadCache(ThreadCacheObject currentThreadCache) {
         // Fetch thread cache over IPC:
         ThreadIPCClient client = getDefaultThreadIPCClient();
         if (client == null || isBackgroundProcess()) {
             return null;
         }
         // Let's get our previous cache object and if necessary sync data :
-        // shouldFetch is set as false to ensure we do not enter into infinite recursion.
-        ThreadCacheObject cache = ThreadCache.get(/*shouldFetch*/ false);
-        if (cache != null) {
+        if (currentThreadCache != null) {
             // Send stored data from thread cache to background process: hit counts, middleware data, ... :
             var updateRes = new UpdateAgentDataCommand.Res(
-                    /* routeHitDeltas */ cache.getRoutes().getDeltaMap(),
-                    /* hitsDelta */ cache.getTotalHits(),
-                    /* middlewareInstalled */ cache.isMiddlewareInstalled()
+                    /* routeHitDeltas */ currentThreadCache.getRoutes().getDeltaMap(),
+                    /* hitsDelta */ currentThreadCache.getTotalHits(),
+                    /* middlewareInstalled */ currentThreadCache.isMiddlewareInstalled()
             );
             new UpdateAgentDataCommand().send(client, updateRes);
         }
