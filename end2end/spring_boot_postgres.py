101e315e5ba0eb3be06e8f2afd2e7996f8d22018
BitterPanda63 <bitterpanda@proton.me>
2025-01-27 09:30:37 +0100

    Update API Call to /api/requests/get

--- end2end/spring_boot_postgres.py
+++ end2end/spring_boot_postgres.py
@@ -28,7 +28,7 @@ test_payloads_path_variables(payloads_exec, urls, route="/api/commands/execute/"
 print("✅ Path variables and Command Injection Tested")
 
 # Test SSRF :
-test_safe_vs_unsafe_payloads(payloads_ssrf, urls, route="/api/requests") # This makes 4 requests and asserts their status codes
+test_safe_vs_unsafe_payloads(payloads_ssrf, urls, route="/api/requests/get") # This makes 4 requests and asserts their status codes
 print("✅ SSRF Tested")
 
 # Test path traversal :
