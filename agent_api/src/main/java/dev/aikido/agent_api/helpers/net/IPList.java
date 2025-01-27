9994063a72e05beaf9e16542f2c4a05e832abaa6
BitterPanda63 <bitterpanda@proton.me>
2025-01-27 11:10:25 +0100

    Rename IPList and it's functionalities

--- agent_api/src/main/java/dev/aikido/agent_api/helpers/net/IPList.java
+++ agent_api/src/main/java/dev/aikido/agent_api/helpers/net/IPList.java
@@ -1,20 +1,21 @@
 package dev.aikido.agent_api.helpers.net;
 
-import inet.ipaddr.AddressStringException;
 import inet.ipaddr.IPAddress;
 import inet.ipaddr.IPAddressString;
-import inet.ipaddr.IPAddressNetwork;
-import java.util.HashSet;
+
 import java.util.List;
 import java.util.ArrayList;
 
-public class BlockList {
+/** IPList
+ * is used for blocklists, allowlists, ... e.g. geo ip restrictions.
+ */
+public class IPList {
     //private HashSet<String> blockedAddresses; // Both IPv4 and IPv6
-    private List<IPAddress> blockedList;
+    private List<IPAddress> ipList;
 
-    public BlockList() {
+    public IPList() {
         //this.blockedAddresses = new HashSet<>();
-        this.blockedList = new ArrayList<>();
+        this.ipList = new ArrayList<>();
     }
 
     public void add(String ipOrCIDR) {
@@ -24,11 +25,11 @@ public class BlockList {
             ip = ip.toPrefixBlock();
         }
         if (ip != null) {
-            blockedList.add(ip);
+            ipList.add(ip);
         }
     }
 
-    public boolean isBlocked(String ip) {
+    public boolean contains(String ip) {
         IPAddressString ipAddressString = new IPAddressString(ip);
         if (!ipAddressString.isValid()) {
             return false; // Invalid IP address
@@ -36,7 +37,7 @@ public class BlockList {
         IPAddress ipAddress = ipAddressString.getAddress();
 
         // Check if the IP address is in any of the blocked subnets
-        for (IPAddress subnet : blockedList) {
+        for (IPAddress subnet : ipList) {
             if (subnet.contains(ipAddress)) {
                 return true;
             }
