fb7d5633e6bf08aa2bcee16768e62d039c628b14
BitterPanda63 <bitterpanda@proton.me>
2025-01-27 09:32:28 +0100

    Also add end2end tests for kotlin & groovy

--- end2end/spring_mvc_postgres_kotlin.py
+++ end2end/spring_mvc_postgres_kotlin.py
@@ -8,11 +8,29 @@ payloads_exec = {
     "safe": "Johhny",
     "unsafe": "'; sleep 2; # "
 }
+payloads_ssrf = {
+    "safe": { "url": "https://aikido.dev/" },
+    "unsafe": { "url": "http://localhost:5000" }
+}
+payloads_path_traversal = {
+    "safe": { "fileName": "HtmlController.java" },
+    "unsafe": { "fileName": "./../../admin.txt" }
+}
 urls = {
     "disabled": "http://localhost:8093",
     "enabled":  "http://localhost:8092"
 }
 
 test_safe_vs_unsafe_payloads(payloads, urls, route="/api/pets/create") # This makes 4 requests and asserts their status codes
+print("✅ SQL Tested")
 # Test if it can block attacks coming from a path variable:
 test_payloads_path_variables(payloads_exec, urls, route="/api/commands/execute/")
+print("✅ Path variables and Command Injection Tested")
+
+# Test SSRF :
+test_safe_vs_unsafe_payloads(payloads_ssrf, urls, route="/api/requests/get") # This makes 4 requests and asserts their status codes
+print("✅ SSRF Tested")
+
+# Test path traversal :
+test_safe_vs_unsafe_payloads(payloads_path_traversal, urls, route="/api/files/read") # This makes 4 requests and asserts their status codes
+print("✅ Path Traversal Tested")
