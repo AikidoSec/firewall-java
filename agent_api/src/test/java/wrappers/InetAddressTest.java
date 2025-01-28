3cea4577ba32c5566e138e7ceefca8575c29d86f
BitterPanda63 <bitterpanda@proton.me>
2025-01-28 11:38:01 +0100

    Remove incorrect tests with thread cache null

--- agent_api/src/test/java/wrappers/InetAddressTest.java
+++ agent_api/src/test/java/wrappers/InetAddressTest.java
@@ -91,17 +91,6 @@ public class InetAddressTest {
     @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "invalid-token-2")
     @SetEnvironmentVariable(key = "AIKIDO_BLOCK", value = "true")
     @Test
-    public void testSSRFWithoutPortAndWithoutThreadCache() throws Exception {
-        setContextAndLifecycle("http://localhost:80");
-        ThreadCache.set(null);
-        assertThrows(ConnectException.class, () -> {
-            fetchResponse("http://localhost/weirdroute");
-        });
-    }
-
-    @SetEnvironmentVariable(key = "AIKIDO_TOKEN", value = "invalid-token-2")
-    @SetEnvironmentVariable(key = "AIKIDO_BLOCK", value = "true")
-    @Test
     public void testSSRFWithHttpClient() {
         setContextAndLifecycle("http://localhost:5000/");
 
