0159547e1cc78d382cd69e2eb4ad8f956e852425
BitterPanda63 <bitterpanda@proton.me>
2025-01-31 16:58:36 +0100

    Uncomment e2e tests

--- end2end/spring_webflux_postgres.py
+++ end2end/spring_webflux_postgres.py
@@ -18,11 +18,11 @@ urls = {
 test_safe_vs_unsafe_payloads(payloads, urls, route="/api/pets/create")
 print("✅ Tested safe/unsafe payloads on /api/create")
 
-# Test blocklists (WIP -> Writing response support)
-#test_ip_blocking("http://localhost:8090/")
-#print("✅ Tested IP Blocking")
-#test_bot_blocking("http://localhost:8090/")
-#print("✅ Tested bot blocking")
+# Test blocklists
+test_ip_blocking("http://localhost:8090/")
+print("✅ Tested IP Blocking")
+test_bot_blocking("http://localhost:8090/")
+print("✅ Tested bot blocking")
 
 # Test ratelimiting (we can use a header to set user) :
 test_ratelimiting("http://localhost:8090/test_ratelimiting_1")
@@ -30,6 +30,6 @@ print("✅ Tested rate-limiting")
 test_ratelimiting_per_user("http://localhost:8090/test_ratelimiting_1")
 print("✅ Tested rate-limiting per user")
 
-# Test path variables : (WIP -> Path Variable support)
+# Test path variables :
 test_payloads_path_variables(payloads_exec, urls, route="/api/commands/execute/")
 print("✅ Tested attack using path variables.")
