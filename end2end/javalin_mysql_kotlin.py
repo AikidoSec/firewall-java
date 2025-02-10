83ec849ff55b020e6fc820b0c3ac1ffeb7b4b16c
Wout Feys <bitterpanda@proton.me>
2025-02-10 15:58:06 +0100

    Create and use a new "app" utility class

--- end2end/javalin_mysql_kotlin.py
+++ end2end/javalin_mysql_kotlin.py
@@ -1,35 +1,28 @@
-from utils.test_safe_vs_unsafe_payloads import test_safe_vs_unsafe_payloads, test_payloads_path_variables
 from javalin_mysql_kotlin.test_sql_attacks import test_sql_attack
 from javalin_postgres.test_ip_blocking import test_ip_blocking
 from javalin_postgres.test_bot_blocking import test_bot_blocking
 from javalin_postgres.test_ratelimiting import test_ratelimiting_per_user, test_ratelimiting
-from utils.EventHandler import EventHandler
+from utils.app import App
+javalin_mysql_app = App(8098)
 
-payloads = {
-    "safe": { "name": "Bobby" },
-    "unsafe": { "name": "Malicious Pet\", \"Gru from the Minions\") -- " }
-}
-payloads_exec = {
-    "safe": "Johhny",
-    "unsafe": "'; sleep 2; # "
-}
-payloads_ssrf = {
-    "safe": { "url": "https://aikido.dev/" },
-    "unsafe": { "url": "http://localhost:5000" },
-}
-payloads_path_traversal = {
-    "safe": { "fileName": "README.md" },
-    "unsafe": { "fileName": "./../databases/docker-compose.yml" },
-}
-urls = {
-    "disabled": "http://localhost:8099",
-    "enabled": "http://localhost:8098"
-}
-
-event_handler = EventHandler()
-event_handler.reset()
-test_safe_vs_unsafe_payloads(payloads, urls, route="/api/create") # Test MySQL+Javalin compat
-print("✅ Tested safe/unsafe payloads on /api/create")
+javalin_mysql_app.add_payload(
+    "sql", route="/api/create",
+    safe={ "name":"Bobby" }, unsafe={ "name": "Malicious Pet\", \"Gru from the Minions\") -- " },
+    test_event=test_sql_attack
+)
+javalin_mysql_app.add_payload(
+    "ssrf", route="/api/request",
+    safe={ "url": "https://aikido.dev/" }, unsafe={ "url": "http://localhost:5000" }
+)
+javalin_mysql_app.add_payload(
+    "path_traversal", route="/api/read",
+    safe={ "fileName": "README.md" }, unsafe={ "fileName": "./../databases/docker-compose.yml" }
+)
+javalin_mysql_app.add_payload(
+    "command_injection", route="/api/execute/",
+    safe="Johnny", unsafe="'; sleep 2; # ", pathvar=True
+)
+javalin_mysql_app.test_all_payloads()
 
 # Test blocklists :
 test_ip_blocking("http://localhost:8098/")
@@ -42,18 +35,3 @@ test_ratelimiting("http://localhost:8098/test_ratelimiting_1")
 print("✅ Tested rate-limiting")
 test_ratelimiting_per_user("http://localhost:8098/test_ratelimiting_1")
 print("✅ Tested rate-limiting per user")
-
-test_sql_attack(event_handler)
-print("✅ Tested accurate reporting of an attack")
-
-# Test path variables :
-test_payloads_path_variables(payloads_exec, urls, route="/api/execute/")
-print("✅ Tested attack using path variables.")
-
-# Test SSRF :
-test_safe_vs_unsafe_payloads(payloads_ssrf, urls, route="/api/request")
-print("✅ SSRF Tested")
-
-# Test path traversal :
-test_safe_vs_unsafe_payloads(payloads_path_traversal, urls, route="/api/read")
-print("✅ Path Traversal Tested")
