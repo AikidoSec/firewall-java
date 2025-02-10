acfc76fa044edd05e58b820c4c813b18d5208886
Wout Feys <bitterpanda@proton.me>
2025-02-10 16:38:35 +0100

    reformat

--- end2end/javalin_mysql_kotlin.py
+++ end2end/javalin_mysql_kotlin.py
@@ -1,19 +1,19 @@
-from utils import App
 from __init__ import events
+from utils import App
 
 javalin_mysql_app = App(8098)
 javalin_mysql_app.add_payload(
     "sql", route="/api/create",
-    safe={ "name":"Bobby" }, unsafe={ "name": "Malicious Pet\", \"Gru from the Minions\") -- " },
+    safe={"name": "Bobby"}, unsafe={"name": "Malicious Pet\", \"Gru from the Minions\") -- "},
     test_event=events["javalin_mysql_attack"]
 )
 javalin_mysql_app.add_payload(
     "ssrf", route="/api/request",
-    safe={ "url": "https://aikido.dev/" }, unsafe={ "url": "http://localhost:5000" }
+    safe={"url": "https://aikido.dev/"}, unsafe={"url": "http://localhost:5000"}
 )
 javalin_mysql_app.add_payload(
     "path_traversal", route="/api/read",
-    safe={ "fileName": "README.md" }, unsafe={ "fileName": "./../databases/docker-compose.yml" }
+    safe={"fileName": "README.md"}, unsafe={"fileName": "./../databases/docker-compose.yml"}
 )
 javalin_mysql_app.add_payload(
     "command_injection", route="/api/execute/",
--- end2end/javalin_postgres.py
+++ end2end/javalin_postgres.py
@@ -1,10 +1,10 @@
-from utils import App
 from __init__ import events
+from utils import App
 
 javalin_postgres_app = App(8088)
 javalin_postgres_app.add_payload(
     "sql", route="/api/create",
-    safe={ "name": "Bobby" }, unsafe={ "name": "Malicious Pet', 'Gru from the Minions') -- " },
+    safe={"name": "Bobby"}, unsafe={"name": "Malicious Pet', 'Gru from the Minions') -- "},
     test_event=events["javalin_postgres_attack"]
 )
 javalin_postgres_app.add_payload(
