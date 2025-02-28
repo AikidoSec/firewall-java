8d5c4c3db9d5d4455f918263701a60bf9b563b00
Wout Feys <bitterpanda@proton.me>
2025-02-28 16:15:16 +0100

    Add end2end tests for cookie(...)

--- end2end/javalin_postgres.py
+++ end2end/javalin_postgres.py
@@ -18,6 +18,12 @@ javalin_postgres_app.add_payload(
     safe_request=Request(route="/api/execute/Bobby", method='GET'),
     unsafe_request=Request(route="/api/execute/Malicious%20Pet%27%2C%20%27Gru%20from%20the%20Minions%27%29%20--%20", method='GET')
 )
+javalin_postgres_app.add_payload(
+    "path_traversal_via_cookie",
+    # First path in Cookie list gets taken : ctx.cookie(str key)
+    safe_request=Request("/api/read_cookie", method='GET', headers={'Cookie': 'path=home.txt;path=123;path=../secrets/key.txt'}),
+    unsafe_request=Request("/api/read_cookie", method='GET', headers={'Cookie': 'path=../secrets/key.txt;path=home.txt'})
+)
 
 javalin_postgres_app.test_all_payloads()
 javalin_postgres_app.test_blocking()
