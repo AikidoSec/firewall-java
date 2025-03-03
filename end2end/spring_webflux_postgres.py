17c217372a0e317b97dd9a80495f29b349ed7e4e
BitterPanda63 <bitterpanda@proton.me>
2025-03-03 15:16:47 +0100

    Use |sleep as an injection via cookie

--- end2end/spring_webflux_postgres.py
+++ end2end/spring_webflux_postgres.py
@@ -11,23 +11,23 @@ spring_webflux_postgres_app.add_payload("command injection",
     unsafe_request=Request("/api/commands/execute/%27%3B%20sleep%202%3B%20%23%20", method='GET'),
 )
 
-# Test that spring handles cookies correctly: 
+# Test that spring handles cookies correctly:
 spring_webflux_postgres_app.add_payload(
     "path_traversal_via_cookie_singular",
     # First command in Cookie list gets used.
     safe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=john'}),
-    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=%27%3B%20sleep%202%3B%20%23%20'})
+    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=|sleep'})
 )
 spring_webflux_postgres_app.add_payload(
     "path_traversal_via_cookie",
     # First command in Cookie list gets used.
-    safe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=john;command=123;command=%27%3B%20sleep%202%3B%20%23%20'}),
-    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=%27%3B%20sleep%202%3B%20%23%20;command=john'})
+    safe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=john;command=123;command=|sleep'}),
+    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=|sleep;command=john'})
 )
 spring_webflux_postgres_app.add_payload(
     "path_traversal_via_cookie_all_same",
-    safe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=john;command=%27%3B%20sleep%202%3B%20%23%20;command=john'}),
-    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=%27%3B%20sleep%202%3B%20%23%20;command=%27%3B%20sleep%202%3B%20%23%20'}),
+    safe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=john;command=|sleep;command=john'}),
+    unsafe_request=Request("/api/commands/executeFromCookie", method='GET', headers={'Cookie': 'command=|sleep;command=|sleep'}),
 )
 
 spring_webflux_postgres_app.test_all_payloads()
