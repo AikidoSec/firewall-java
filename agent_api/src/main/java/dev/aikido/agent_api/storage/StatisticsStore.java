6b2eda1d0dc38ce1484865fa2b9070761a2c7474
Wout Feys <bitterpanda@proton.me>
2025-03-12 12:44:40 +0100

    Add extra try-catch to statisticsStore and remove the getStats

--- agent_api/src/main/java/dev/aikido/agent_api/storage/StatisticsStore.java
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/StatisticsStore.java
@@ -6,6 +6,7 @@ import dev.aikido.agent_api.helpers.logging.Logger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public final class StatisticsStore {
+    private static final Logger logger = LogManager.getLogger(StatisticsStore.class);
     private static final ReentrantLock mutex = new ReentrantLock();
     private static final Statistics stats = new Statistics();
 
@@ -13,9 +14,14 @@ public final class StatisticsStore {
 
     }
 
-    public static Statistics getStats() {
+    public static Statistics.StatsRecord getStatsRecord() {
+        Statistics.StatsRecord result = null;
         mutex.lock();
-        Statistics result = StatisticsStore.stats;
+        try {
+            result = stats.getRecord();
+        } catch (Throwable e) {
+            logger.debug("An error occurred getting the stats record: %s", e.getMessage());
+        }
         mutex.unlock();
         return result;
     }
