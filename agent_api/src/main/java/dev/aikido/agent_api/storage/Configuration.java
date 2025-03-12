873af8cecd36b635f3aab650c0c5fba1f736bf6d
Wout Feys <bitterpanda@proton.me>
2025-03-12 11:09:19 +0100

    Create a full Configuration object

--- agent_api/src/main/java/dev/aikido/agent_api/storage/Configuration.java
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/Configuration.java
@@ -1,30 +1,150 @@
 package dev.aikido.agent_api.storage;
 
-import java.util.HashSet;
-import java.util.Set;
+import dev.aikido.agent_api.background.Endpoint;
+import dev.aikido.agent_api.background.cloud.api.APIResponse;
+import dev.aikido.agent_api.background.cloud.api.ReportingApi;
+import dev.aikido.agent_api.helpers.net.IPList;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static dev.aikido.agent_api.helpers.IPListBuilder.createIPList;
 
 public class Configuration {
     private boolean blocking;
-    // This is true by default, awaiting the startup event, if the startup event is unsuccessful this will remain true.
     private boolean receivedAnyStats = true;
-    private boolean middlewareInstalled = false;
+    private List<Endpoint> endpoints = new ArrayList<>();
     private Set<String> blockedUserIds = new HashSet<>();
+    private IPList bypassedIPs = new IPList();
 
-    public Configuration() {}
+    // IP restrictions (e.g. Geo-IP Restrictions) :
+    public record IPListEntry(IPList ipList, String description) {
+    }
+
+    private final List<IPListEntry> blockedIps = new ArrayList<>();
+    private final List<IPListEntry> allowedIps = new ArrayList<>();
+    // User-Agent Blocking (e.g. bot blocking) :
+    private Pattern blockedUserAgentRegex;
+
+    public Configuration() {
+    }
+
+    // allowedIps, blockedIps, blockedUserAgentRegex are updated using this function
+    public void updateBlockedLists(Optional<ReportingApi.APIListsResponse> blockedListsRes) {
+        if (!blockedListsRes.isEmpty()) {
+            ReportingApi.APIListsResponse res = blockedListsRes.get();
+            // Update blocked IP addresses (e.g. for geo restrictions) :
+            if (res.blockedIPAddresses() != null) {
+                for (ReportingApi.ListsResponseEntry entry : res.blockedIPAddresses()) {
+                    IPList ipList = createIPList(entry.ips());
+                    this.blockedIps.add(new IPListEntry(ipList, entry.description()));
+                }
+            }
+            // Update allowed IP addresses (e.g. for geo restrictions) :
+            if (res.allowedIPAddresses() != null) {
+                for (ReportingApi.ListsResponseEntry entry : res.allowedIPAddresses()) {
+                    IPList ipList = createIPList(entry.ips());
+                    this.allowedIps.add(new IPListEntry(ipList, entry.description()));
+                }
+            }
+            // Update Blocked User-Agents regex
+            if (res.blockedUserAgents() != null && !res.blockedUserAgents().isEmpty()) {
+                this.blockedUserAgentRegex = Pattern.compile(res.blockedUserAgents(), Pattern.CASE_INSENSITIVE);
+            }
+        }
+    }
+
+    // blocking, endpoints, bypassed ips, blocked user ids are updated using this function
+    public void updateConfig(APIResponse apiResponse) {
+        if (apiResponse == null || !apiResponse.success()) {
+            return;
+        }
+        this.setBlocking(apiResponse.block());
+        this.setBypassedIPs(apiResponse.allowedIPAddresses());
+        this.setBlockedUserIds(apiResponse.blockedUserIds());
+        this.setEndpoints(apiResponse.endpoints());
+        this.setReceivedAnyStats(apiResponse.receivedAnyStats());
+    }
 
     // Blocking status, i.e. should an attack be throwing an error or not
     public boolean isBlocking() {
         return blocking;
     }
+
     public void setBlocking(boolean blocking) {
         this.blocking = blocking;
     }
 
     // blocked user ids: These are users that were blocked in the cloud portal
-    public void setBlockedUserIds(Set<String> blockedUserIds) {
-        this.blockedUserIds = blockedUserIds;
+    public void setBlockedUserIds(List<String> blockedUserIds) {
+        if (blockedUserIds != null) {
+            this.blockedUserIds = new HashSet<>(blockedUserIds);
+        }
     }
+
     public boolean isUserBlocked(String userId) {
         return this.blockedUserIds.contains(userId);
+    }
+
+    // bypassed ips: This IPList contains all IPs that should not be scanned, blocked, etc.
+    public void setBypassedIPs(List<String> bypassedIps) {
+        this.bypassedIPs = createIPList(bypassedIps);
+    }
+
+    public boolean isBypassedIP(String ip) {
+        return this.bypassedIPs.matches(ip);
+    }
+
+    // endpoints: contains rate-limiting data, allowed ips, ...
+    public void setEndpoints(List<Endpoint> endpoints) {
+        if (endpoints != null) {
+            this.endpoints = endpoints;
+        }
+    }
+
+    public List<Endpoint> getEndpoints() {
+        return this.endpoints;
+    }
+
+
+    public record BlockedResult(boolean blocked, String description) {
+    }
+
+    /**
+     * Check if the IP is blocked (e.g. Geo IP Restrictions)
+     * This first checks the global ip allowlist, and afterward the global ip blocklist.
+     */
+    public BlockedResult isIpBlocked(String ip) {
+        for (IPListEntry entry : allowedIps) {
+            if (!entry.ipList.matches(ip)) {
+                return new BlockedResult(true, entry.description);
+            }
+        }
+        for (IPListEntry entry : blockedIps) {
+            if (entry.ipList.matches(ip)) {
+                return new BlockedResult(true, entry.description);
+            }
+        }
+        return new BlockedResult(false, null);
+    }
+
+    /**
+     * Check if a given User-Agent is blocked or not
+     */
+    public boolean isBlockedUserAgent(String userAgent) {
+        if (blockedUserAgentRegex != null) {
+            return blockedUserAgentRegex.matcher(userAgent).find();
+        }
+        return false;
+    }
+
+    // receivedAnyStats: true by default, awaiting the startup event, if the startup
+    // event is unsuccessful this will remain true.
+    public void setReceivedAnyStats(boolean receivedAnyStats) {
+        this.receivedAnyStats = receivedAnyStats;
+    }
+
+    public boolean hasReceivedAnyStats() {
+        return this.receivedAnyStats;
     }
 }
