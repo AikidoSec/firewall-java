95e5ced956f31ca6ce4cbb0ed475a1ef57941623
BitterPanda63 <bitterpanda@proton.me>
2025-03-17 11:09:59 +0100

    Remove BlockingEnabled command

--- agent_api/src/main/java/dev/aikido/agent_api/helpers/ShouldBlockHelper.java
+++ agent_api/src/main/java/dev/aikido/agent_api/helpers/ShouldBlockHelper.java
@@ -1,13 +1,6 @@
 package dev.aikido.agent_api.helpers;
 
-import dev.aikido.agent_api.background.ipc_commands.BlockingEnabledCommand;
-import dev.aikido.agent_api.background.utilities.ThreadIPCClient;
-import dev.aikido.agent_api.background.ipc_commands.Command;
-import dev.aikido.agent_api.helpers.env.BlockingEnv;
-
-import java.util.Optional;
-
-import static dev.aikido.agent_api.background.utilities.ThreadIPCClientFactory.getDefaultThreadIPCClient;
+import static dev.aikido.agent_api.storage.ConfigStore.getConfig;
 
 public final class ShouldBlockHelper {
     private ShouldBlockHelper() {}
@@ -17,17 +10,6 @@ public final class ShouldBlockHelper {
      * @return true if the attack should be blocked
      */
     public static boolean shouldBlock() {
-        ThreadIPCClient client = getDefaultThreadIPCClient();
-        if (client == null) {
-            // Fallback on environment variable :
-            return new BlockingEnv().getValue();
-        }
-        Optional<BlockingEnabledCommand.Res> res = new BlockingEnabledCommand().send(client, new Command.EmptyResult());
-        if (!res.isEmpty()) {
-            return res.get().isBlockingEnabled();
-        }
-
-        // Fallback on environment variable :
-        return new BlockingEnv().getValue();
+        return getConfig().isBlockingEnabled();
     }
 }
--- agent_api/src/test/java/background/ipc_commands/CommandRouterTest.java
+++ agent_api/src/test/java/background/ipc_commands/CommandRouterTest.java
@@ -1,15 +1,11 @@
 package background.ipc_commands;
 
-import com.google.gson.Gson;
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
-import dev.aikido.agent_api.background.ipc_commands.BlockingEnabledCommand;
 import dev.aikido.agent_api.background.ipc_commands.CommandRouter;
-import dev.aikido.agent_api.storage.ConfigStore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -38,28 +34,5 @@ public class CommandRouterTest {
     public void testNonExistingCommand() {
         Optional<byte[]> result = commandRouter.parseIPCInput("INVALID_COMMAND${}".getBytes(StandardCharsets.UTF_8));
         assertTrue(result.isEmpty());
-    }
-
-    @Test
-    public void testShouldBlockCommand() throws IOException {
-        byte[] blockingTrue = new Gson()
-                .toJson(new BlockingEnabledCommand.Res(true))
-                .getBytes(StandardCharsets.UTF_8);
-        byte[] blockingFalse = new Gson()
-                .toJson(new BlockingEnabledCommand.Res(false))
-                .getBytes(StandardCharsets.UTF_8);
-
-        ConfigStore.updateBlocking(true);
-        Optional<byte[]> result = commandRouter.parseIPCInput("BLOCKING_ENABLED${}".getBytes(StandardCharsets.UTF_8));
-
-        assertTrue(result.isPresent());
-        assertArrayEquals(blockingTrue, result.get());
-
-
-        ConfigStore.updateBlocking(false);
-        Optional<byte[]> result2 = commandRouter.parseIPCInput("BLOCKING_ENABLED${}".getBytes(StandardCharsets.UTF_8));
-
-        assertTrue(result2.isPresent());
-        assertArrayEquals(blockingFalse, result2.get());
     }
 }
