afeda9b2cb70bdadf8cd98cf3a2c60f27813759d
Wout Feys <bitterpanda@proton.me>
2025-03-17 13:37:01 +0100

    Add log saying reading from queue... to AttackQueueConsumerTask

--- agent_api/src/main/java/dev/aikido/agent_api/background/AttackQueueConsumerTask.java
+++ agent_api/src/main/java/dev/aikido/agent_api/background/AttackQueueConsumerTask.java
@@ -2,14 +2,16 @@ package dev.aikido.agent_api.background;
 
 import dev.aikido.agent_api.background.cloud.CloudConnectionManager;
 import dev.aikido.agent_api.background.cloud.api.events.APIEvent;
+import dev.aikido.agent_api.helpers.logging.LogManager;
+import dev.aikido.agent_api.helpers.logging.Logger;
 
 import java.util.TimerTask;
-import java.util.concurrent.BlockingQueue;
 
 import static dev.aikido.agent_api.vulnerabilities.AttackQueueStore.getAttackFromQueue;
 
 public class AttackQueueConsumerTask extends TimerTask {
     private final CloudConnectionManager connectionManager;
+    private final static Logger logger = LogManager.getLogger(AttackQueueConsumerTask.class);
     public AttackQueueConsumerTask(CloudConnectionManager connectionManager) {
         this.connectionManager = connectionManager;
     }
@@ -18,6 +20,7 @@ public class AttackQueueConsumerTask extends TimerTask {
     public void run() {
         APIEvent event = getAttackFromQueue();
         while (event != null) {
+            logger.trace("reading from queue...");
             this.connectionManager.reportEvent(event, false /* should not update config */);
             event = getAttackFromQueue(); // refresh
         }
