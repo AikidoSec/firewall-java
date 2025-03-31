79229723e4597df84a2d24f80253e80d2cf469ff
BitterPanda63 <bitterpanda@proton.me>
2025-03-31 10:11:30 +0200

    Create UsersStore

---
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/UsersStore.java
@@ -0,0 +1,41 @@
+package dev.aikido.agent_api.storage;
+
+import dev.aikido.agent_api.context.User;
+import dev.aikido.agent_api.helpers.logging.LogManager;
+import dev.aikido.agent_api.helpers.logging.Logger;
+
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+public final class UsersStore {
+    private static final Logger logger = LogManager.getLogger(UsersStore.class);
+    private static final ReentrantLock mutex = new ReentrantLock();
+    private static final Users users = new Users();
+
+    private UsersStore() {
+
+    }
+
+    public static List<User> getUsersAsList() {
+        mutex.lock();
+        List<User> result = users.asList();
+        mutex.unlock();
+        return result;
+    }
+
+    public static void addUser(User user) {
+        mutex.lock();
+        try {
+            users.addUser(user);
+        } catch (Throwable e) {
+            logger.debug("Error occurred while adding user: %s", e.getMessage());
+        }
+        mutex.unlock();
+    }
+
+    public static void clear() {
+        mutex.lock();
+        users.clear();
+        mutex.unlock();
+    }
+}
