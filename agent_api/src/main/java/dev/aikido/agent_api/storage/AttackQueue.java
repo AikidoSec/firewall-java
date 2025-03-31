2c75727713c7c9f764ea67914a5d16cb6b086ff2
BitterPanda63 <bitterpanda@proton.me>
2025-03-31 15:45:35 +0200

    Remove AttackCommand IPC in favour of attackDetected

--- agent_api/src/main/java/dev/aikido/agent_api/storage/AttackQueue.java
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/AttackQueue.java
@@ -1,11 +1,15 @@
 package dev.aikido.agent_api.storage;
 
 import dev.aikido.agent_api.background.cloud.api.events.APIEvent;
+import dev.aikido.agent_api.context.ContextObject;
 import dev.aikido.agent_api.helpers.logging.LogManager;
 import dev.aikido.agent_api.helpers.logging.Logger;
+import dev.aikido.agent_api.vulnerabilities.Attack;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import static dev.aikido.agent_api.background.cloud.api.events.DetectedAttack.createAPIEvent;
 
 public final class AttackQueue {
     private static final Logger logger = LogManager.getLogger(AttackQueue.class);
@@ -13,6 +17,18 @@ public final class AttackQueue {
 
     private AttackQueue() {
 
+    }
+
+    public static void attackDetected(Attack attack, ContextObject context) {
+        // increment statistics
+        StatisticsStore.incrementAttacksDetected();
+        if (ServiceConfigStore.getConfig().isBlockingEnabled()) {
+            StatisticsStore.incrementAttacksBlocked(); // Also increment blocked attacks.
+        }
+
+        // generate attack API event
+        APIEvent detectedAttack = createAPIEvent(attack, context);
+        add(detectedAttack);
     }
 
     public static void add(APIEvent attack) {
