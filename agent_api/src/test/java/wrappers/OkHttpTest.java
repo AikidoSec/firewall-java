cf98cb5add922c5237f88b13ec0e6723ea0740e6
BitterPanda63 <bitterpanda@proton.me>
2025-04-28 10:41:19 +0200

    Expand OkHttp test cases

--- agent_api/src/test/java/wrappers/OkHttpTest.java
+++ agent_api/src/test/java/wrappers/OkHttpTest.java
@@ -1,6 +1,8 @@
 package wrappers;
 
 import dev.aikido.agent_api.context.Context;
+import dev.aikido.agent_api.storage.Hostnames;
+import dev.aikido.agent_api.storage.HostnamesStore;
 import dev.aikido.agent_api.storage.ServiceConfigStore;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,6 +14,7 @@ import utils.EmptySampleContextObject;
 
 import java.io.IOException;
 import java.net.ConnectException;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -22,6 +25,7 @@ public class OkHttpTest {
     @AfterEach
     void cleanup() {
         Context.set(null);
+        HostnamesStore.clear();
     }
 
     @BeforeEach
@@ -37,6 +41,7 @@ public class OkHttpTest {
 
     @Test
     public void testSSRFLocalhostValid() throws Exception {
+        assertEquals(0, getHits("localhost", 5000));
         setContextAndLifecycle("http://localhost:5000");
 
         RuntimeException exception1 = assertThrows(RuntimeException.class, () -> {
@@ -45,6 +50,8 @@ public class OkHttpTest {
         assertEquals(
                 "Aikido Zen has blocked a server-side request forgery",
                 exception1.getMessage());
+        assertEquals(1, getHits("localhost", 5000));
+
 
         RuntimeException exception2 = assertThrows(RuntimeException.class, () -> {
             fetchResponse("http://localhost:5000");
@@ -52,6 +59,8 @@ public class OkHttpTest {
         assertEquals(
                 "Aikido Zen has blocked a server-side request forgery",
                 exception2.getMessage());
+        assertEquals(2, getHits("localhost", 5000));
+
 
         RuntimeException exception3 = assertThrows(RuntimeException.class, () -> {
             fetchResponse("https://localhost:5000/api/test");
@@ -59,24 +68,33 @@ public class OkHttpTest {
         assertEquals(
                 "Aikido Zen has blocked a server-side request forgery",
                 exception3.getMessage());
+        assertEquals(3, getHits("localhost", 5000));
+
     }
 
     @Test
     public void testSSRFWithoutPort() throws Exception {
         setContextAndLifecycle("http://localhost:80");
+        assertEquals(0, getHits("localhost", 80));
+
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
             fetchResponse("http://localhost/api/test");
         });
         assertEquals("Aikido Zen has blocked a server-side request forgery", exception.getMessage());
+        assertEquals(1, getHits("localhost", 80));
+
     }
 
     @Test
     public void testSSRFWithoutPortAndWithoutContext() throws Exception {
+        assertEquals(0, getHits("localhost", 80));
+
         setContextAndLifecycle("http://localhost:80");
         Context.set(null);
         assertThrows(ConnectException.class, () -> {
             fetchResponse("http://localhost/api/test");
         });
+        assertEquals(1, getHits("localhost", 80));
     }
 
     private void fetchResponse(String urlString) throws IOException {
@@ -85,5 +103,13 @@ public class OkHttpTest {
                 .addHeader("Authorization", "Bearer invalid-token-2")
                 .build();
         Response response = client.newCall(request).execute();
+    }
+    private int getHits(String hostname, int port) {
+        for (Hostnames.HostnameEntry entry: Objects.requireNonNull(HostnamesStore.getHostnamesAsList())) {
+            if (entry.getHostname().equals(hostname) && entry.getPort() == port) {
+                return entry.getHits();
+            }
+        }
+        return 0;
     }
 }
