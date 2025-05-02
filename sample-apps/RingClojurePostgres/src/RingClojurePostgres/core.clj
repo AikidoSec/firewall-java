7e5d5cac9b59d386afbbe7cf200335622dd630f8
BitterPanda <bitterpanda@proton.me>
2025-05-02 11:28:36 +0200

    Update app to serve some HTML and some simple responses

--- sample-apps/RingClojurePostgres/src/RingClojurePostgres/core.clj
+++ sample-apps/RingClojurePostgres/src/RingClojurePostgres/core.clj
@@ -1,11 +1,24 @@
 (ns RingClojurePostgres.core
-  (:use ring.adapter.jetty))
+    (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.util.response :refer [response]]))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello World"})
+(defn html-handler [file]
+    (let [html-content (slurp file)]
+        (response html-content)))
+
+(def app
+    (fn [request]
+        (case (:uri request)
+            "/" (html-handler "resources/index.html")
+            "/pages/create" (html-handler "resources/create.html")
+            "/pages/execute" (html-handler "resources/execute_command.html")
+            "/pages/request" (html-handler "resources/request.html")
+            "/pages/read" (html-handler "resources/read_file.html")
+            ; Test rate-limiting
+            "/test_ratelimiting_1" {:body "Hello" :status 200}
+
+            {:body "404 Not Found" :status 404})))
 
 (defn -main
-  [& args]
-  (run-jetty handler {:port 8102}))
+    [& args]
+    (run-jetty app {:port 8102 :join? false}))
