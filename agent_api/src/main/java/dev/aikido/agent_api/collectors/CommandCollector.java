6d6d8493da1b866aa70ae0934249217c0d8b0eed
BitterPanda <bitterpanda@proton.me>
2025-06-26 19:05:21 +0200

    Add even more detailed trace logs for wrappers

--- agent_api/src/main/java/dev/aikido/agent_api/collectors/CommandCollector.java
+++ agent_api/src/main/java/dev/aikido/agent_api/collectors/CommandCollector.java
@@ -1,17 +1,25 @@
 package dev.aikido.agent_api.collectors;
 
+import dev.aikido.agent_api.helpers.logging.LogManager;
+import dev.aikido.agent_api.helpers.logging.Logger;
 import dev.aikido.agent_api.storage.statistics.OperationKind;
 import dev.aikido.agent_api.storage.statistics.StatisticsStore;
 import dev.aikido.agent_api.vulnerabilities.Scanner;
 import dev.aikido.agent_api.vulnerabilities.Vulnerabilities;
+import org.slf4j.LoggerFactory;
 
 public final class CommandCollector {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(CommandCollector.class);
+
     private CommandCollector() {}
+    private static final Logger logger = LogManager.getLogger(CommandCollector.class);
     public static void report(Object command) {
         if (command instanceof String commandStr) {
             if (commandStr.isEmpty()) {
                 return; // Empty command, don't scan.
             }
+
+            logger.trace("Scanning command: %s", commandStr);
 
             // report stats
             StatisticsStore.registerCall("runtime.Exec", OperationKind.EXEC_OP);
