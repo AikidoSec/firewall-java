fb67397d81fdfbb831f5776cfaa92dd61ac3fccb
BitterPanda63 <bitterpanda@proton.me>
2025-06-27 03:03:00 +0200

    rename startDaemon to startAikidoDaemon

--- agent_api/src/main/java/dev/aikido/agent_api/StartDaemon.java
+++ agent_api/src/main/java/dev/aikido/agent_api/StartDaemon.java
@@ -8,16 +8,16 @@ import dev.aikido.agent_api.helpers.logging.Logger;
 public final class StartDaemon {
     private static final Logger logger = LogManager.getLogger(StartDaemon.class);
 
-    public static void startDaemon() {
+    public static void startAikidoDaemon() {
         Token token = Token.fromEnv();
         if (token == null) {
             logger.info("Failed to start background process due to an invalid token");
             return;
         }
-        startDaemon(token);
+        startAikidoDaemon(token);
     }
 
-    public static void startDaemon(Token token) {
+    public static void startAikidoDaemon(Token token) {
         BackgroundProcess backgroundProcess = new BackgroundProcess("main-background-process", token);
         backgroundProcess.setDaemon(true);
         backgroundProcess.start();
