adf084c7fea1c0459a7054be06fef2d00ac54da1
Hans Ott <hansott@hotmail.be>
2025-07-30 15:13:50 +0200

    Calculate bytes using UTF-8 encoding

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/sql_injection/RustSQLInterface.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/sql_injection/RustSQLInterface.java
@@ -2,6 +2,7 @@ package dev.aikido.agent_api.vulnerabilities.sql_injection;
 
 import jnr.ffi.LibraryLoader;
 import jnr.ffi.LibraryOption;
+import jnr.ffi.annotations.Encoding;
 import dev.aikido.agent_api.helpers.logging.LogManager;
 import dev.aikido.agent_api.helpers.logging.Logger;
 
@@ -9,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 import static dev.aikido.agent_api.vulnerabilities.sql_injection.GetBinaryPath.getPathForBinary;
 
@@ -16,16 +18,19 @@ public final class RustSQLInterface {
     private RustSQLInterface() {}
 
     private static final Logger logger = LogManager.getLogger(RustSQLInterface.class);
+
+    @Encoding("UTF-8")
     public interface SqlLib {
         int detect_sql_injection(String query, long queryLen, String userinput, long userinputLen, int dialect);
     }
+
     public static boolean detectSqlInjection(String query, String userInput, Dialect dialect) {
         int dialectInteger = dialect.getDialectInteger();
         try {
             SqlLib lib = loadLibrary();
             if (lib != null) {
-                long queryLen = query != null ? query.length() : 0;
-                long userInputLen = userInput != null ? userInput.length() : 0;
+                long queryLen = query != null ? query.getBytes(StandardCharsets.UTF_8).length : 0;
+                long userInputLen = userInput != null ? userInput.getBytes(StandardCharsets.UTF_8).length : 0;
                 int result = lib.detect_sql_injection(query, queryLen, userInput, userInputLen, dialectInteger);
                 return result == 1;
             }
