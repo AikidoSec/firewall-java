d8a6c5d98d6524206687129e3a7f189c25bf43d9
BitterPanda <bitterpanda@proton.me>
2025-09-05 16:48:17 +0200

    use the containsIgnoreCase & add extra test cases

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/attack_wave_detection/PathChecker.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/attack_wave_detection/PathChecker.java
@@ -2,6 +2,7 @@ package dev.aikido.agent_api.vulnerabilities.attack_wave_detection;
 
 import dev.aikido.agent_api.helpers.ArrayHelpers;
 
+import static dev.aikido.agent_api.helpers.ArrayHelpers.containsIgnoreCase;
 import static dev.aikido.agent_api.vulnerabilities.attack_wave_detection.Paths.*;
 
 public final class PathChecker {
@@ -14,14 +15,14 @@ public final class PathChecker {
         String filename = ArrayHelpers.lastElement(segments);
 
         if (filename != null) {
-            if (FILE_NAMES.contains(filename)) {
+            if (containsIgnoreCase(FILE_NAMES, filename)) {
                 return true;
             }
             // Check file extension
             int dotIndex = filename.lastIndexOf('.');
             if (dotIndex != -1) {
                 String ext = filename.substring(dotIndex + 1);
-                if (FILE_EXTENSIONS.contains(ext)) {
+                if (containsIgnoreCase(FILE_EXTENSIONS, ext)) {
                     return true;
                 }
             }
@@ -29,7 +30,7 @@ public final class PathChecker {
 
         // Check all directory names
         for (int i = 0; i < segments.length - 1; i++) {
-            if (DIRECTORY_NAMES.contains(segments[i])) {
+            if (containsIgnoreCase(DIRECTORY_NAMES, segments[i])) {
                 return true;
             }
         }
