1de31e075867363f7071e81bc0199b10da94a601
bitterpanda <bitterpanda@proton.me>
2025-09-15 14:07:02 +0000

    Update agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/StoredSSRFException.java
    Co-authored-by: Hans Ott <hansott@hotmail.be>

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/StoredSSRFException.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/ssrf/StoredSSRFException.java
@@ -3,9 +3,6 @@ package dev.aikido.agent_api.vulnerabilities.ssrf;
 import dev.aikido.agent_api.vulnerabilities.AikidoException;
 
 public class StoredSSRFException extends AikidoException {
-    public StoredSSRFException(String msg) {
-        super(msg);
-    }
 
     public static StoredSSRFException get() {
         String defaultMsg = generateDefaultMessage("a stored server-side request forgery");
