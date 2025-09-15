294b8ed3e3a28fbf42ea9447f5d772ed0394d4e0
BitterPanda <bitterpanda@proton.me>
2025-09-15 15:46:35 +0200

    catch update: both SSRFException & StoredSSRFException

--- agent_api/src/main/java/dev/aikido/agent_api/collectors/DNSRecordCollector.java
+++ agent_api/src/main/java/dev/aikido/agent_api/collectors/DNSRecordCollector.java
@@ -74,7 +74,7 @@ public final class DNSRecordCollector {
                     throw SSRFException.get();
                 }
             }
-        } catch (SSRFException e) {
+        } catch (SSRFException | StoredSSRFException e) {
             throw e;
         } catch (Throwable e) {
             logger.trace(e);
