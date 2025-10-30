583c4fc4d620aa756a65b3e84125e349db9e3a17
BitterPanda <bitterpanda@proton.me>
2025-10-30 14:48:44 +0100

    Create Packages class to store all packages before reporting to core

---
+++ agent_api/src/main/java/dev/aikido/agent_api/storage/Packages.java
@@ -0,0 +1,31 @@
+package dev.aikido.agent_api.storage;
+
+import java.util.*;
+
+public class Packages {
+    public record PackageInfo(String name, String version, long requiredAt) {}
+    private Map<String, List<PackageInfo>> packages = new HashMap<>();
+
+    public void addPackage(String name, String version) {
+        List<PackageInfo> versions = packages.computeIfAbsent(name, k -> new ArrayList<>());
+        for (PackageInfo pkg : versions) {
+            if (pkg.version().equals(version)) {
+                return;
+            }
+        }
+        versions.add(new PackageInfo(name, version, System.currentTimeMillis()));
+    }
+
+    public List<PackageInfo> asArray() {
+        List<PackageInfo> result = new ArrayList<>();
+        for (List<PackageInfo> versionList : packages.values()) {
+            result.addAll(versionList);
+        }
+        return result;
+    }
+
+    public void clear() {
+        packages.clear();
+    }
+}
+
