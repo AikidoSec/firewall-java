8a70bb606fc79412404105849c093f60f2870126
BitterPanda <bitterpanda@proton.me>
2025-11-21 10:30:00 +0100

    Update PathTraversalDetectorTest cases to use this new INSTANCE

--- agent_api/src/test/java/vulnerabilities/path_traversal/PathTraversalDetectorTest.java
+++ agent_api/src/test/java/vulnerabilities/path_traversal/PathTraversalDetectorTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PathTraversalDetectorTest {
-    private final PathTraversalDetector pathTraversalDetector = new PathTraversalDetector();
     private void assertNotAttack(Detector.DetectorResult detectorResult) {
         assertFalse(detectorResult.isDetectedAttack());
     }
@@ -16,199 +15,199 @@ public class PathTraversalDetectorTest {
     }
     @Test
     public void testEmptyUserInput() {
-        assertNotAttack(pathTraversalDetector.run("", new String[]{"test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("", new String[]{"test.txt"}));
     }
 
     @Test
     public void testEmptyFileInput() {
-        assertNotAttack(pathTraversalDetector.run("test", new String[]{""}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test", new String[]{""}));
     }
 
     @Test
     public void testEmptyUserInputAndFileInput() {
-        assertNotAttack(pathTraversalDetector.run("", new String[]{""}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("", new String[]{""}));
     }
 
     @Test
     public void testUserInputIsASingleCharacter() {
-        assertNotAttack(pathTraversalDetector.run("t", new String[]{"test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("t", new String[]{"test.txt"}));
     }
 
     @Test
     public void testFileInputIsASingleCharacter() {
-        assertNotAttack(pathTraversalDetector.run("test", new String[]{"t"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test", new String[]{"t"}));
     }
 
     @Test
     public void testSameAsUserInput() {
-        assertNotAttack(pathTraversalDetector.run("text.txt", new String[]{"text.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("text.txt", new String[]{"text.txt"}));
     }
 
     @Test
     public void testWithDirectoryBefore() {
-        assertNotAttack(pathTraversalDetector.run("text.txt", new String[]{"directory/text.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("text.txt", new String[]{"directory/text.txt"}));
     }
 
     @Test
     public void testWithBothDirectoryBefore() {
-        assertNotAttack(pathTraversalDetector.run("directory/text.txt", new String[]{"directory/text.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("directory/text.txt", new String[]{"directory/text.txt"}));
     }
 
     @Test
     public void testUserInputAndFileInputAreSingleCharacters() {
-        assertNotAttack(pathTraversalDetector.run("t", new String[]{"t"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("t", new String[]{"t"}));
     }
 
     @Test
     public void testItFlagsDotDotSlash() {
-        assertAttack(pathTraversalDetector.run("../", new String[]{"../test.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("../", new String[]{"../test.txt"}));
     }
 
     @Test
     public void testItFlagsDotDotSlashSlash() {
-        assertAttack(pathTraversalDetector.run("..//", new String[]{"..//test.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("..//", new String[]{"..//test.txt"}));
     }
 
     @Test
     public void testItFlagsMoreComplexPaths() {
-        assertAttack(pathTraversalDetector.run("..//secrets/key.txt", new String[]{"resources/blog/..//secrets/key.txt"}));
-        assertAttack(pathTraversalDetector.run(".././/secrets/key.txt", new String[]{"resources/blog/.././/secrets/key.txt"}));
-        assertAttack(pathTraversalDetector.run("////../secrets/key.txt", new String[]{"resources/blog/////../secrets/key.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("..//secrets/key.txt", new String[]{"resources/blog/..//secrets/key.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run(".././/secrets/key.txt", new String[]{"resources/blog/.././/secrets/key.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("////../secrets/key.txt", new String[]{"resources/blog/////../secrets/key.txt"}));
     }
 
     @Test
     public void testItFlagsBackslashDotDot() {
-        assertAttack(pathTraversalDetector.run("..\\", new String[]{"..\\test.txt",}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("..\\", new String[]{"..\\test.txt",}));
     }
 
     @Test
     public void testItFlagsDoubleDotSlash() {
-        assertAttack(pathTraversalDetector.run("../../", new String[]{"../../test.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("../../", new String[]{"../../test.txt"}));
     }
 
     @Test
     public void testItFlagsDoubleDotBackslash() {
-        assertAttack(pathTraversalDetector.run("..\\..\\", new String[]{"..\\..\\test.txt",}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("..\\..\\", new String[]{"..\\..\\test.txt",}));
     }
 
     @Test
     public void testItFlagsFourDotSlash() {
-        assertAttack(pathTraversalDetector.run("../../../../", new String[]{"../../../../test.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("../../../../", new String[]{"../../../../test.txt"}));
     }
 
     @Test
     public void testItFlagsThreeDotBackslash() {
-        assertAttack(pathTraversalDetector.run("..\\..\\..\\", new String[]{"..\\..\\..\\test.txt",}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("..\\..\\..\\", new String[]{"..\\..\\..\\test.txt",}));
     }
 
     @Test
     public void testUserInputIsLongerThanFilePath() {
-        assertNotAttack(pathTraversalDetector.run("../../file.txt", new String[]{"../file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("../../file.txt", new String[]{"../file.txt"}));
     }
 
     @Test
     public void testAbsoluteLinuxPath() {
-        assertAttack(pathTraversalDetector.run("/etc/passwd", new String[]{"/etc/passwd"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("/etc/passwd", new String[]{"/etc/passwd"}));
     }
 
     @Test
     public void testLinuxUserDirectory() {
-        assertAttack(pathTraversalDetector.run("/home/user/", new String[]{"/home/user/file.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("/home/user/", new String[]{"/home/user/file.txt"}));
     }
 
     @Test
     public void testWindowsDriveLetter() {
-        assertAttack(pathTraversalDetector.run("C:\\", new String[]{"C:\\file.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("C:\\", new String[]{"C:\\file.txt"}));
     }
 
     @Test
     public void testNoPathTraversal() {
-        assertNotAttack(pathTraversalDetector.run("/storage/file.txt", new String[]{"/appdata/storage/file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("/storage/file.txt", new String[]{"/appdata/storage/file.txt"}));
     }
 
     @Test
     public void testDoesNotFlagTest() {
-        assertNotAttack(pathTraversalDetector.run("test", new String[]{"/app/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test", new String[]{"/app/test.txt"}));
     }
 
     @Test
     public void testDoesNotFlagExampleTestTxt() {
-        assertNotAttack(pathTraversalDetector.run("example/test.txt", new String[]{"/app/data/example/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("example/test.txt", new String[]{"/app/data/example/test.txt"}));
     }
 
     @Test
     public void testDoesNotAbsolutePathWithDifferentFolder() {
-        assertNotAttack(pathTraversalDetector.run("/etc/hack/config", new String[]{"/etc/app/config"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("/etc/hack/config", new String[]{"/etc/app/config"}));
     }
 
     @Test
     public void testDoesNotAbsolutePathInsideAnotherFolder() {
-        assertNotAttack(pathTraversalDetector.run("/etc/config", new String[]{"/etc/app/data/etc/config"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("/etc/config", new String[]{"/etc/app/data/etc/config"}));
     }
 
     @Test
     public void testUserInputContainsMultipleUnsafePathParts() {
-        assertNotAttack(pathTraversalDetector.run("../../../", new String[]{"directory/../../file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("../../../", new String[]{"directory/../../file.txt"}));
     }
 
     @Test
     public void testUserInputWithMixedPathSeparators() {
-        assertNotAttack(pathTraversalDetector.run("..\\..\\", new String[]{"directory/../file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("..\\..\\", new String[]{"directory/../file.txt"}));
     }
 
     @Test
     public void testUserInputWithEncodedCharacters() {
-        assertAttack(pathTraversalDetector.run("%2E%2E/../", new String[]{"directory/%2E%2E/../file.txt"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("%2E%2E/../", new String[]{"directory/%2E%2E/../file.txt"}));
     }
 
     @Test
     public void testUserInputWithEncodedBackslash() {
-        assertNotAttack(pathTraversalDetector.run("%5C%5C", new String[]{"directory/%5C%5Cfile.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("%5C%5C", new String[]{"directory/%5C%5Cfile.txt"}));
     }
 
     @Test
     public void testUserInputWithSpaces() {
-        assertNotAttack(pathTraversalDetector.run("test file", new String[]{"directory/test file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test file", new String[]{"directory/test file.txt"}));
     }
 
     @Test
     public void testUserInputWithLeadingSpaces() {
-        assertNotAttack(pathTraversalDetector.run(" test.txt", new String[]{"directory/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run(" test.txt", new String[]{"directory/test.txt"}));
     }
 
     @Test
     public void testUserInputWithTrailingSpaces() {
-        assertNotAttack(pathTraversalDetector.run("test.txt ", new String[]{"directory/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test.txt ", new String[]{"directory/test.txt"}));
     }
 
     @Test
     public void testUserInputWithSpecialCharacters() {
-        assertNotAttack(pathTraversalDetector.run("test@file.txt", new String[]{"directory/test@file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test@file.txt", new String[]{"directory/test@file.txt"}));
     }
 
     @Test
     public void testUserInputWithAbsolutePath() {
-        assertAttack(pathTraversalDetector.run("/etc/passwd", new String[]{"/etc/passwd"}));
+        assertAttack(PathTraversalDetector.INSTANCE.run("/etc/passwd", new String[]{"/etc/passwd"}));
     }
 
     @Test
     public void testUserInputWithMixedCase() {
-        assertNotAttack(pathTraversalDetector.run("Test.txt", new String[]{"directory/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("Test.txt", new String[]{"directory/test.txt"}));
     }
 
     @Test
     public void testUserInputWithLongPath() {
         String longUserInput = "a".repeat(260); // Assuming a long input
-        assertNotAttack(pathTraversalDetector.run(longUserInput, new String[]{"directory/test.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run(longUserInput, new String[]{"directory/test.txt"}));
     }
 
     @Test
     public void testUserInputWithEmptyFilePath() {
-        assertNotAttack(pathTraversalDetector.run("test", new String[]{""}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test", new String[]{""}));
     }
 
     @Test
     public void testUserInputWithFilePathContainingSpaces() {
-        assertNotAttack(pathTraversalDetector.run("test file", new String[]{"directory/test file.txt"}));
+        assertNotAttack(PathTraversalDetector.INSTANCE.run("test file", new String[]{"directory/test file.txt"}));
     }
 }
