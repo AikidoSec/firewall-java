8f172bf33c169f397bdc0f7e77d2d632bae07275
BitterPanda <bitterpanda@proton.me>
2025-11-21 12:50:40 +0100

    Update RuntimeExecWrapper to make it more explicit

--- agent/src/main/java/dev/aikido/agent/wrappers/RuntimeExecWrapper.java
+++ agent/src/main/java/dev/aikido/agent/wrappers/RuntimeExecWrapper.java
@@ -14,8 +14,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 import static net.bytebuddy.implementation.bytecode.assign.Assigner.Typing.DYNAMIC;
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static net.bytebuddy.matcher.ElementMatchers.nameContains;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class RuntimeExecWrapper implements Wrapper {
     public String getName() {
@@ -24,8 +23,11 @@ public class RuntimeExecWrapper implements Wrapper {
         return CommandExecAdvice.class.getName();
     }
     public ElementMatcher<? super MethodDescription> getMatcher() {
+        // We only monkey-patch Runtime.exec(string), technically all Runtime.exec calls end up at the ProcessBuilder
+        // but at that point they are already split into arguments, so scanning a single command requires us to add
+        // a wrapper here, only for single strings.
         return ElementMatchers.isDeclaredBy(Runtime.class)
-                .and(ElementMatchers.nameContainsIgnoreCase("exec"));
+                .and(ElementMatchers.nameContainsIgnoreCase("exec")).and(takesArgument(0,  String.class));
     }
 
     @Override
@@ -39,13 +41,8 @@ public class RuntimeExecWrapper implements Wrapper {
         // To bypass this issue we load collectors from a .jar file.
         @Advice.OnMethodEnter
         public static void before(
-                @Advice.This(typing = DYNAMIC, optional = true) Object target,
-                @Advice.Origin Executable method,
-                @Advice.Argument(0) Object argument
+            @Advice.Argument(value = 0, typing = DYNAMIC) String command
         ) throws Throwable {
-            if (!(argument instanceof String)) {
-                return;
-            }
             String jarFilePath = System.getProperty("AIK_agent_api_jar");
             URLClassLoader classLoader = null;
             try {
@@ -63,7 +60,7 @@ public class RuntimeExecWrapper implements Wrapper {
                 // Run report with "argument"
                 for (Method method2: clazz.getMethods()) {
                     if(method2.getName().equals("report")) {
-                        method2.invoke(null, argument);
+                        method2.invoke(null, command);
                         break;
                     }
                 }
