41f4a3f5c18a13f6581ba036ec33d3a4d3f17096
BitterPanda <bitterpanda@proton.me>
2025-11-25 15:29:48 +0100

    Update StringsFromContext to extract Content-Disposition

--- agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/StringsFromContext.java
+++ agent_api/src/main/java/dev/aikido/agent_api/vulnerabilities/StringsFromContext.java
@@ -1,9 +1,15 @@
 package dev.aikido.agent_api.vulnerabilities;
 
 import dev.aikido.agent_api.context.ContextObject;
+import dev.aikido.agent_api.helpers.ContentDispositionFilename;
 import dev.aikido.agent_api.helpers.extraction.StringExtractor;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import static dev.aikido.agent_api.context.ContextObject.getHeader;
 
 public class StringsFromContext {
     private final Map<String, String> bodyStrings;
@@ -15,7 +21,15 @@ public class StringsFromContext {
     public StringsFromContext(ContextObject contextObject) {
         bodyStrings = loadFromCache(contextObject, "body", contextObject.getBody());
         queryStrings = loadFromCache(contextObject, "query", contextObject.getQuery());
-        headersStrings = loadFromCache(contextObject, "headers", contextObject.getHeaders());
+
+        HashMap<String, List<String>> headers = new HashMap<>(contextObject.getHeaders());
+
+        // parse special headers like Content-Disposition to already extract certain values
+        Optional<String> filename = ContentDispositionFilename.extract(contextObject.getHeader("Content-Disposition"));
+        filename.ifPresent(s -> headers.put("Content-Disposition[filename]", List.of(s)));
+
+        headersStrings = loadFromCache(contextObject, "headers", headers);
+
         cookieStrings = loadFromCache(contextObject, "cookies", contextObject.getCookies());
         routeParamStrings = loadFromCache(contextObject, "routeParams", contextObject.getParams());
     }
