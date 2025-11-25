cdd4579d3d6b337a6fa941e6a480f8f00c5e2bcd
BitterPanda <bitterpanda@proton.me>
2025-11-25 14:00:49 +0100

    Update ContentDispositionFilename code

--- agent_api/src/main/java/dev/aikido/agent_api/helpers/ContentDispositionFilename.java
+++ agent_api/src/main/java/dev/aikido/agent_api/helpers/ContentDispositionFilename.java
@@ -1,17 +1,34 @@
 package dev.aikido.agent_api.helpers;
 
+import dev.aikido.agent_api.helpers.logging.LogManager;
+import dev.aikido.agent_api.helpers.logging.Logger;
+
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class ContentDispositionFilename {
     private ContentDispositionFilename() {}
+    private static final Logger logger = LogManager.getLogger(ContentDispositionFilename.class);
 
     public static Optional<String> extractFilenameFromHeader(String contentDisposition) {
-        ContentDispositionHeader.ParseResult res = ContentDispositionHeader.parse(contentDisposition);
+        try {
+            ContentDispositionHeader.ParseResult res = ContentDispositionHeader.parse(contentDisposition);
 
-        if (res.params() == null) {
-            return Optional.empty();
+            if (res.params() == null) {
+                return Optional.empty();
+            }
+
+            // filename* is preferred over filename;
+            if (!res.params().getOrDefault("filename*", "").isEmpty()) {
+                return Optional.of(res.params().get("filename*"));
+            }
+
+            if (!res.params().getOrDefault("filename", "").isEmpty()) {
+                return Optional.of(res.params().get("filename"));
+            }
+        } catch(RuntimeException e) {
+            logger.debug("Error while parsing content disposition header for filename: %s", e.getMessage());
         }
 
         return Optional.empty();
